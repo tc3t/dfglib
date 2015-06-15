25d86fa07c244abdfa1c5cd1a3a5b98a93b3e5e5
tc3t <tc3t@users.noreply.github.com>
2015-06-15 18:56:17 +0300

    [imp] IsContiguousMemoryIterator now detects std::wstring::iterator.

--- dfg/iter/iscontiguousmemoryiterator.hpp
+++ dfg/iter/iscontiguousmemoryiterator.hpp
@@ -9,25 +9,27 @@
 
 DFG_ROOT_NS_BEGIN
 {
-	template <class Iter_T> struct IsStdArrayContainer{ static const bool value = false; };
+    template <class Iter_T> struct IsStdArrayContainer{ static const bool value = false; };
 
-	template <class Elem_T, size_t N> struct IsStdArrayContainer<std::array<Elem_T, N>>
-	{
-		static const bool value = true;
-	};
+    template <class Elem_T, size_t N> struct IsStdArrayContainer<std::array<Elem_T, N>>
+    {
+        static const bool value = true;
+    };
 
-	// Note: Does not find all contiguous memory iterators, for example iterators of std::array<int, N> are not detected as CMI.
-	// TODO: Make check more reliable and implement at least the std::array.
-	// For related standard proposal, see "N4132: Contiguous Iterators" (http://isocpp.org/files/papers/n4132.html)
-	template <class Iter_T>
-	struct IsContiguousMemoryIterator
-	{
-		typedef typename std::iterator_traits<Iter_T>::value_type value_type;
-		static const bool value = std::is_pointer<Iter_T>::value == true ||
-									std::is_same<Iter_T, typename std::vector<value_type>::iterator>::value == true ||
-									std::is_same<Iter_T, typename std::vector<value_type>::const_iterator>::value == true ||
-									std::is_same<Iter_T, typename std::string::iterator>::value == true ||
-									std::is_same<Iter_T, typename std::string::const_iterator>::value == true;
-	};
+    // Note: Does not find all contiguous memory iterators, for example iterators of std::array<int, N> are not detected as CMI.
+    // TODO: Make check more reliable and implement at least the std::array.
+    // For related standard proposal, see "N4132: Contiguous Iterators" (http://isocpp.org/files/papers/n4132.html)
+    template <class Iter_T>
+    struct IsContiguousMemoryIterator
+    {
+        typedef typename std::iterator_traits<Iter_T>::value_type value_type;
+        static const bool value = std::is_pointer<Iter_T>::value == true ||
+                                    std::is_same<Iter_T, typename std::vector<value_type>::iterator>::value == true ||
+                                    std::is_same<Iter_T, typename std::vector<value_type>::const_iterator>::value == true ||
+                                    std::is_same<Iter_T, typename std::string::iterator>::value == true ||
+                                    std::is_same<Iter_T, typename std::string::const_iterator>::value == true ||
+                                    std::is_same<Iter_T, typename std::wstring::iterator>::value == true ||
+                                    std::is_same<Iter_T, typename std::wstring::const_iterator>::value == true;
+    };
 
 } // namespace
