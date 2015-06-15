f8bc9602e857503c1520f3c89c93bab9ea799d27
tc3t <tc3t@users.noreply.github.com>
2015-06-15 19:04:09 +0300

    [new] os::TemporaryFileStream

---
+++ dfg/os/TemporaryFileStream.hpp
@@ -0,0 +1,188 @@
+#pragma once
+
+#include "../dfgDefs.hpp"
+#include <random>
+#include "../utf.hpp"
+#include "../iter/szIterator.hpp"
+#include "removeFile.hpp"
+
+#ifdef _WIN32
+    #include "win.hpp"
+#endif // _WIN32
+
+DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(os)
+{
+
+#ifdef _WIN32
+        /* Creates temporary files guaranteeing std::ostream access to the created temp file.
+        Related code
+            -QTemporaryFile
+            -std::tmpnam (http://en.cppreference.com/w/cpp/io/c/tmpnam)
+            -std::tmpfile (http://en.cppreference.com/w/cpp/io/c/tmpfile)
+        TODO: wchar_t -> char conversions assume that every wchar_t is an individual code point; while it should work in vast majority of cases, this is not correct.
+        */
+        class DFG_CLASS_NAME(TemporaryFileStream)
+        {
+        public:
+            typedef wchar_t CharT;
+            typedef std::basic_string<CharT> StringT;
+
+            DFG_CLASS_NAME(TemporaryFileStream)()
+            {
+                init(nullptr, nullptr, nullptr, nullptr);
+            }
+
+            DFG_CLASS_NAME(TemporaryFileStream)(const char* pPszFolderPath,
+                const char* pPszNamePrefix,
+                const char* pPszNameSuffix,
+                const char* pPszExtensionWithOrWithoutDot,
+                const size_t nRandomNameChars = 8)
+            {
+                const auto charToWcharConverter = [](const char* psz) -> std::wstring
+                                                    {
+                                                        //return DFG_MODULE_NS(utf)::utf8ToFixedChSizeStr<CharT>(makeSzRange(psz));
+                                                        return std::wstring(psz, psz + std::strlen(psz)); // Convert from latin1.
+                                                    };
+                StringT sFolderPath = (pPszFolderPath) ? charToWcharConverter(pPszFolderPath) : StringT();
+                StringT sNamePrefix = (pPszNamePrefix) ? charToWcharConverter(pPszNamePrefix) : StringT();
+                StringT sNameSuffix = (pPszNameSuffix) ? charToWcharConverter(pPszNameSuffix) : StringT();
+                StringT sExt = (pPszExtensionWithOrWithoutDot) ? charToWcharConverter(pPszExtensionWithOrWithoutDot) : StringT();
+                init((pPszFolderPath) ? sFolderPath.c_str() : nullptr,
+                    (pPszNamePrefix) ? sNamePrefix.c_str() : nullptr,
+                    (pPszNameSuffix) ? sNameSuffix.c_str() : nullptr,
+                    (pPszExtensionWithOrWithoutDot) ? sExt.c_str() : nullptr,
+                    nRandomNameChars);
+            }
+
+            DFG_CLASS_NAME(TemporaryFileStream)(const wchar_t* pPszFolderPath,
+                const wchar_t* pPszNamePrefix,
+                const wchar_t* pPszNameSuffix,
+                const wchar_t* pPszExtensionWithOrWithoutDot,
+                const size_t nRandomNameChars = 8)
+            {
+                init(pPszFolderPath, pPszNamePrefix, pPszNameSuffix, pPszExtensionWithOrWithoutDot, nRandomNameChars);
+            }
+
+            void init(const wchar_t* pPszFolderPath = nullptr,
+                const wchar_t* pPszNamePrefix = nullptr,
+                const wchar_t* pPszNameSuffix = nullptr,
+                const wchar_t* pPszExtensionWithOrWithoutDot = nullptr,
+                const size_t nRandomNameChars = 8)
+            {
+                for (int i = 0; i < 10; ++i) // Try 10 times in case that opening fails; in default case highly unexpected because of 8 random chars.
+                {
+                    StringT sFilename;
+                    if (pPszNamePrefix)
+                        sFilename = pPszNamePrefix;
+                    sFilename += generateRandomFileNamePart(nRandomNameChars);
+                    if (pPszNameSuffix)
+                        sFilename += pPszNameSuffix;
+                    if (pPszExtensionWithOrWithoutDot && *pPszExtensionWithOrWithoutDot != '\0')
+                    {
+                        if (*pPszExtensionWithOrWithoutDot == '.')
+                            pPszExtensionWithOrWithoutDot++;
+                        sFilename.append(1, '.');
+                        sFilename += pPszExtensionWithOrWithoutDot;
+                    }
+                    StringT sPath = (pPszFolderPath) ? pPszFolderPath : tempFolderPath();
+                    if (!sPath.empty() && sPath.back() != '\\')
+                        sPath += '\\';
+                    sPath += sFilename;
+#ifdef _MSC_VER
+                    m_ostrm.open(sPath, std::ios::binary | std::ios::trunc); // MSVC has wstring overload.
+#else
+                    m_ostrm.open(DFG_MODULE_NS(utf)::codePointsToUtf8(sPath), std::ios::binary | std::ios::trunc); // TODO: check what encoding open() should receive; for now use UTF8.
+#endif
+                    if (m_ostrm.is_open() && m_ostrm.good())
+                    {
+                        m_sPath = std::move(sPath);
+                        break;
+                    }
+                    else
+                        m_ostrm.clear();
+                }
+            }
+
+            ~DFG_CLASS_NAME(TemporaryFileStream)()
+            {
+                close();
+            }
+
+            void close()
+            {
+                m_ostrm.close();
+                if (!m_sPath.empty())
+                    removeFile(m_sPath.c_str());
+            }
+
+            template <class T>
+            DFG_CLASS_NAME(TemporaryFileStream)& operator<<(const T& obj)
+            {
+                stream() << obj;
+                return *this;
+            }
+
+            void flush()
+            {
+                stream().flush();
+            }
+
+            std::string pathU8() const { return DFG_MODULE_NS(utf)::codePointsToUtf8(m_sPath); }
+            std::string pathLatin1() const { return DFG_MODULE_NS(utf)::codePointsToLatin1(m_sPath); }
+            std::wstring pathW() const { return m_sPath; }
+
+            StringT path() const { return m_sPath; }
+
+            std::ofstream& stream() { return m_ostrm; }
+
+            bool isOpen() const { return m_ostrm.is_open(); }
+
+            static StringT generateRandomFileNamePart(const int nCountRequest = 8)
+            {
+                StringT s;
+                std::random_device rd;
+                std::uniform_int_distribution<int> uid(0, 35);
+                const auto nEffectiveCount = Min(nCountRequest, 64); // Allow 64 random chars at max (rather arbitrary choice, change if need be).
+                for (int i = 0; i < nEffectiveCount; ++i)
+                {
+                    const auto val = uid(rd);
+                    if (val < 10)
+                        s.append(1, static_cast<char>(48 + val)); // Numbers 0-9
+                    else
+                        s.append(1, static_cast<char>(97 + val - 10)); // small letters a-z
+                }
+                return s;
+            }
+
+            /* Returns temp folder path ended with separator char.
+            Related code
+                -QDir::tempPath
+                -boost::filesystem::temp_directory_path
+                -GetTempPath() (Windows)
+            */
+            template <class Char_T>
+            static std::basic_string<Char_T> tempFolderPathT()
+            {
+                return DFG_SUB_NS_NAME(win)::GetTempPathT<Char_T>();
+            }
+
+            static std::string tempFolderPathC()
+            {
+                return tempFolderPathT<char>();
+            }
+
+            static std::wstring tempFolderPathW()
+            {
+                return tempFolderPathT<wchar_t>();
+            }
+
+            static StringT tempFolderPath()
+            {
+                return tempFolderPathT<CharT>();
+            }
+
+            std::ofstream m_ostrm;
+            StringT m_sPath;
+        };
+#endif // _WIN32
+}} // module namespace
--- dfgTest/dfgTestOs.cpp
+++ dfgTest/dfgTestOs.cpp
@@ -3,30 +3,104 @@
 #include <dfg/str.hpp>
 #include <dfg/io.hpp>
 #include <dfg/os/memoryMappedFile.hpp>
+#include <dfg/os/TemporaryFileStream.hpp>
+#include <dfg/alg.hpp>
+#include <dfg/dfgBase.hpp>
 
-TEST(DfgOs, pathFindExtension)
+TEST(dfgOs, pathFindExtension)
 {
-	using namespace DFG_MODULE_NS(os);
-	using namespace DFG_MODULE_NS(str);
-	EXPECT_EQ(strCmp(".txt", pathFindExtension("c:/testFolder/test.txt")), 0);
-	EXPECT_EQ(strCmp(".txt", pathFindExtension("c:\\testFolder\\test.txt")), 0);
-	EXPECT_EQ(strCmp(L".", pathFindExtension(L"c:/testFolder/test.")), 0);
-	EXPECT_EQ(strCmp(L".", pathFindExtension(L"c:\\testFolder\\test.")), 0);
-	EXPECT_EQ(strCmp(L"", pathFindExtension(L"c:/testFolder/test")), 0);
-	EXPECT_EQ(strCmp(L".extension", pathFindExtension(L"z:/testFolder/test/dfg/dfgvcbcv/bcvbb/.extension")), 0);
-	EXPECT_EQ(strCmp(L".extension", pathFindExtension(L"z:\\testFolder\\test\\dfg\\dfgvcbcv\\bcvbb\\.extension")), 0);
-	EXPECT_NE(strCmp(L".extension", pathFindExtension(L"z:\\testFolder\\test\\dfg\\dfgvcbcv\\bcvbb\\.extensionA")), 0);
+    using namespace DFG_MODULE_NS(os);
+    using namespace DFG_MODULE_NS(str);
+    EXPECT_EQ(strCmp(".txt", pathFindExtension("c:/testFolder/test.txt")), 0);
+    EXPECT_EQ(strCmp(".txt", pathFindExtension("c:\\testFolder\\test.txt")), 0);
+    EXPECT_EQ(strCmp(L".", pathFindExtension(L"c:/testFolder/test.")), 0);
+    EXPECT_EQ(strCmp(L".", pathFindExtension(L"c:\\testFolder\\test.")), 0);
+    EXPECT_EQ(strCmp(L"", pathFindExtension(L"c:/testFolder/test")), 0);
+    EXPECT_EQ(strCmp(L".extension", pathFindExtension(L"z:/testFolder/test/dfg/dfgvcbcv/bcvbb/.extension")), 0);
+    EXPECT_EQ(strCmp(L".extension", pathFindExtension(L"z:\\testFolder\\test\\dfg\\dfgvcbcv\\bcvbb\\.extension")), 0);
+    EXPECT_NE(strCmp(L".extension", pathFindExtension(L"z:\\testFolder\\test\\dfg\\dfgvcbcv\\bcvbb\\.extensionA")), 0);
 }
 
-TEST(DfgOs, MemoryMappedFile)
+TEST(dfgOs, MemoryMappedFile)
 {
-	using namespace DFG_MODULE_NS(os);
-	using namespace DFG_MODULE_NS(io);
-	DFG_CLASS_NAME(MemoryMappedFile) mmf;
-	mmf.open("testfiles/matrix_3x3.txt");
-	EXPECT_TRUE(mmf.is_open());
-	EXPECT_TRUE(mmf.size() == 62);
-	std::vector<char> bytes(mmf.begin(), mmf.end());
-	const auto vec2 = fileToVector("testfiles/matrix_3x3.txt");
-	EXPECT_EQ(bytes, vec2);
+    using namespace DFG_MODULE_NS(os);
+    using namespace DFG_MODULE_NS(io);
+    DFG_CLASS_NAME(MemoryMappedFile) mmf;
+    mmf.open("testfiles/matrix_3x3.txt");
+    EXPECT_TRUE(mmf.is_open());
+    EXPECT_TRUE(mmf.size() == 62);
+    std::vector<char> bytes(mmf.begin(), mmf.end());
+    const auto vec2 = fileToVector("testfiles/matrix_3x3.txt");
+    EXPECT_EQ(bytes, vec2);
+}
+
+TEST(dfgOs, TemporaryFile)
+{
+    using namespace DFG_MODULE_NS(os);
+
+    std::string sTfPath;
+    std::string sTfWPath;
+
+    {
+        DFG_CLASS_NAME(TemporaryFileStream) tf(nullptr, nullptr, nullptr, "dfgTestTemp", 16);
+        DFG_CLASS_NAME(TemporaryFileStream) tfW(nullptr, nullptr, nullptr, L"dfgTestTemp", 16);
+        EXPECT_TRUE(tf.isOpen());
+        EXPECT_TRUE(tfW.isOpen());
+
+        sTfPath = tf.pathLatin1();
+        sTfWPath = tfW.pathLatin1();
+        EXPECT_EQ(sTfPath, tf.pathU8()); // Hack: assume that path is ANSI, probably true in most cases, but not guaranteed.
+        EXPECT_EQ(sTfWPath, tfW.pathU8()); // Hack: assume that path is ANSI, probably true in most cases, but not guaranteed.
+
+        EXPECT_TRUE(isPathFileAvailable(sTfPath.c_str(), FileModeExists));
+        EXPECT_TRUE(isPathFileAvailable(sTfWPath.c_str(), FileModeExists));
+
+        const char szTestText[] = "test_text_to_temp_file";
+
+        {
+            tf << szTestText;
+            tf.flush();
+            std::ifstream istrm(sTfPath);
+            EXPECT_TRUE(istrm.is_open());
+            std::string sRead;
+            istrm >> sRead;
+            istrm.close();
+            EXPECT_EQ(szTestText, sRead);
+        }
+
+        {
+            tfW << szTestText;
+            tfW.flush();
+            std::ifstream istrmW(sTfWPath);
+            EXPECT_TRUE(istrmW.is_open());
+            std::string sRead;
+            istrmW >> sRead;
+            istrmW.close();
+            EXPECT_EQ(szTestText, sRead);
+        }
+    }
+
+    EXPECT_FALSE(isPathFileAvailable(sTfPath.c_str(), FileModeExists));
+    EXPECT_FALSE(isPathFileAvailable(sTfWPath.c_str(), FileModeExists));
+
+    // Test temp file path which has character outside latin1
+    {
+        const wchar_t cEuroSign[2] = { 0x20AC , 0 };
+        DFG_CLASS_NAME(TemporaryFileStream) tf(nullptr, cEuroSign, nullptr, L"dfgTestTemp");
+        const auto sPath = tf.path();
+        const auto sPathU8 = tf.pathU8();
+        const auto sPathLatin1 = tf.pathLatin1();
+        const auto i = DFG_MODULE_NS(alg)::indexOf(sPath, 0x20AC);
+        EXPECT_TRUE(DFG_ROOT_NS::isValidIndex(sPath, i));
+        { // Hack: assume that the path is otherwise ANSI.
+            EXPECT_EQ(sPath.size() + 2, sPathU8.size());
+            EXPECT_EQ(sPath.size(), sPathLatin1.size());
+            auto utf8EuroSignRange = DFG_ROOT_NS::makeRange(sPathU8.begin() + i, sPathU8.begin() + i + 3);
+
+            const auto sEuro = DFG_MODULE_NS(utf)::utf8ToFixedChSizeStr<wchar_t>(utf8EuroSignRange);
+            EXPECT_EQ(1, sEuro.size());
+            EXPECT_EQ(0x20AC, sEuro.front());
+        }
+        EXPECT_TRUE(isPathFileAvailable(sPath.c_str(), FileModeExists));
+    }
 }
