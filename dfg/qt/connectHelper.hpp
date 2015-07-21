8750efa9694e2dee6a0961ea32378e06be1fb097
tc3t <tc3t@users.noreply.github.com>
2015-07-21 20:42:37 +0300

    [ref] DFG_QT_VERIFY_CONNECT generated warning in release builds.

--- dfg/qt/connectHelper.hpp
+++ dfg/qt/connectHelper.hpp
@@ -3,4 +3,8 @@
 #include "../dfgDefs.hpp"
 #include "../dfgAssert.hpp"
 
-#define DFG_QT_VERIFY_CONNECT(x) DFG_VERIFY_WITH_MSG(static_cast<bool>(x), "Qt connect failed.");
+#ifdef _DEBUG
+    #define DFG_QT_VERIFY_CONNECT(x) DFG_VERIFY_WITH_MSG(static_cast<bool>(x), "Qt connect failed."); // This causes warning C4552 ("operator has no effect") in release builds.
+#else
+    #define DFG_QT_VERIFY_CONNECT(x) x
+#endif
