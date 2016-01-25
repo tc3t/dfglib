f766d9e1ed61abd6757cc997a890283f52c4a5a6
tc3t <tc3t@users.noreply.github.com>
2016-01-25 19:43:58 +0200

    [imp] cont::SortedSequence: Added replace() and erase().

--- dfg/cont/SortedSequence.hpp
+++ dfg/cont/SortedSequence.hpp
@@ -3,6 +3,7 @@
 #include "../dfgDefs.hpp"
 #include "ElementType.hpp"
 #include "../alg/sortSingleItem.hpp"
+#include <algorithm>
 
 DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(cont) {
 
@@ -12,6 +13,7 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(cont) {
     public:
         typedef typename DFG_MODULE_NS(cont)::DFG_CLASS_NAME(ElementType)<Cont_T>::type ValueT;
         typedef typename Cont_T::size_type size_type;
+		typedef typename Cont_T::iterator contTIterator;
         typedef typename Cont_T::const_iterator contTConstIterator;
         typedef typename Cont_T::value_type value_type;
 
@@ -26,6 +28,24 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(cont) {
             m_cont.reserve(n);
         }
 
+		// TODO: test
+		void replace(const value_type& old, const value_type& newVal)
+		{
+			auto iter = find(old);
+			if (iter == m_cont.end())
+				return;
+			*iter = newVal;
+			::DFG_MODULE_NS(alg)::sortSingleItem(m_cont, iter);
+		}
+
+		// TODO: test
+		void erase(const value_type& val)
+		{
+			auto iter = find(val);
+			if (iter != m_cont.end())
+				m_cont.erase(iter);
+		}
+
         size_type size() const { return m_cont.size(); }
 
         contTConstIterator begin()  const   { return m_cont.begin();  }
@@ -33,6 +53,14 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(cont) {
         contTConstIterator end()    const   { return m_cont.end();    }
         contTConstIterator cend()   const   { return m_cont.cend();   }
 
+	private:
+		contTIterator find(const value_type& val)
+		{
+			auto iter = std::lower_bound(m_cont.begin(), m_cont.end(), val);
+			return (iter != m_cont.end() && *iter == val) ? iter : m_cont.end();
+		}
+
+	public:
         Cont_T m_cont;
     };
 
