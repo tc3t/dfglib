b253c08e3dbdc0d2df61279eba382bae2d81ffc2
tc3t <tc3t@users.noreply.github.com>
2016-07-13 17:25:13 +0300

    [build] CsvItemModelExample wouldn't build due to changed strLen().

--- dfg/str/strlen.hpp
+++ dfg/str/strlen.hpp
@@ -14,8 +14,10 @@ DFG_ROOT_NS_BEGIN { DFG_SUB_NS(str) {
 // strLen(s) and strLen(s.c_str()) may return different value.
 // Note also that complexity may be different for different types:
 // linear for null terminated strings, constant for objects that store their size like std::string.
-inline size_t strLen(NonNullCStr psz)           {return std::strlen(psz);}
-inline size_t strLen(NonNullCStrW psz)          {return std::wcslen(psz);}
+inline size_t strLen(NonNullCStr psz)           { return std::strlen(psz); }
+inline size_t strLen(char* psz)                 { return std::strlen(psz); }
+inline size_t strLen(NonNullCStrW psz)          { return std::wcslen(psz); }
+inline size_t strLen(wchar_t* psz)              { return std::wcslen(psz); }
 
 inline size_t strLen(const SzPtrAsciiR& tpsz)   { return strLen(tpsz.c_str()); }
 inline size_t strLen(const SzPtrLatin1R& tpsz)  { return strLen(tpsz.c_str()); }
