395b683cbdf493621cf0815b17d23452e9fc9424
tc3t <tc3t@users.noreply.github.com>
2016-11-19 19:28:44 +0200

    [ref] str: moved strCmp() to it's own file.

--- dfg/str.hpp
+++ dfg/str.hpp
@@ -4,6 +4,7 @@
 #include "dfgBase.hpp"
 #include <cstring>
 #include <string>
+#include "str/strCmp.hpp"
 #include "str/strlen.hpp"
 #include "ReadOnlySzParam.hpp"
 #include <boost/lexical_cast.hpp>
@@ -84,19 +85,6 @@ std::wstring toStrW(const T& obj)
 
 template <size_t N> inline char* strCpy(char (&dest)[N], NonNullCStr pszSrc) {return strcpy(dest, pszSrc);}
 template <size_t N> inline wchar_t* strCpy(wchar_t (&dest)[N], NonNullCStrW pszSrc) {return wcscpy(dest, pszSrc);}
-
-// Wrappers for strcmp and wsccmp.
-// Lexicographic("dictionary-like") comparison. For example: "a" < "b", "111" < "15").
-// Note thought that for example "B" < "a".
-// [return] : If psz1 < psz2: negative value
-//			  If psz1 == psz2 : 0
-//			  If psz1 > psz2 : positive value
-inline int strCmp(const NonNullCStr psz1, const NonNullCStr psz2)	{return strcmp(psz1, psz2);}
-inline int strCmp(const NonNullCStrW psz1, const NonNullCStrW psz2) {return wcscmp(psz1, psz2);}
-template <class Char_T, CharPtrType Type_T> inline int strCmp(const SzPtrT<Char_T, Type_T>& tpsz1, const SzPtrT<Char_T, Type_T>& tpsz2)
-{ 
-    return strCmp(tpsz1.c_str(), tpsz2.c_str());
-}
 
 // Replaces occurrences of 'sOldSub' with 'sNewSub' in given string.
 // Note: Behaviour is undefined if substrings overlap with 'str'.
---
+++ dfg/str/strCmp.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstring>
+#include "../dfgBaseTypedefs.hpp"
+#include "../SzPtrTypes.hpp"
+
+DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(str) {
+
+    // Wrappers for strcmp and wsccmp.
+    // Lexicographic("dictionary-like") comparison. For example: "a" < "b", "111" < "15").
+    // Note thought that for example "B" < "a".
+    // [return] : If psz1 < psz2: negative value
+    //			  If psz1 == psz2 : 0
+    //			  If psz1 > psz2 : positive value
+    inline int strCmp(const NonNullCStr psz1, const NonNullCStr psz2)   { return std::strcmp(psz1, psz2); }
+    inline int strCmp(const NonNullCStrW psz1, const NonNullCStrW psz2) { return std::wcscmp(psz1, psz2); }
+    template <class Char_T, CharPtrType Type_T> inline int strCmp(const SzPtrT<Char_T, Type_T>& tpsz1, const SzPtrT<Char_T, Type_T>& tpsz2)
+    {
+        return strCmp(tpsz1.c_str(), tpsz2.c_str());
+    }
+
+} } // module namespace
