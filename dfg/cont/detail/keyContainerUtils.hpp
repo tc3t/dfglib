94b9ea4e74296c0ab9a18f79a46597c643b1ff21
tc3t <tc3t@users.noreply.github.com>
2017-04-17 00:04:06 +0300

    [imp] cont::MapVector&SetVector: when searching const char* from std::string-keys, now a StringViewC is constructed which may especially in linear searches result in much better performance. It is also possible to provide user-defined search type specializations.

--- dfg/cont/detail/keyContainerUtils.hpp
+++ dfg/cont/detail/keyContainerUtils.hpp
@@ -2,8 +2,50 @@
 
 #include "../../dfgDefs.hpp"
 #include "../../alg/find.hpp"
+#include "../../ReadOnlySzParam.hpp"
+#include <type_traits>
 
 DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(cont) { namespace DFG_DETAIL_NS {
+
+/* KeySearchTypeToInternalSearchType defines the type that is used when searching keys of type 'Key_T' using search param 'Search_T'.
+ By default the type is 'Search_T'. Users can add their specializations if needed for example using the following as starting point:
+ namespace DFG_ROOT_NS
+ {
+    namespace DFG_SUB_NS_NAME(cont)
+    {
+        namespace DFG_DETAIL_NS
+        {
+            template <> struct KeySearchTypeToInternalSearchType<KeyType, SearchType> { typedef CustomSearchType type; };
+        }
+    }
+ }
+ */
+template <class Key_T, class Search_T> struct KeySearchTypeToInternalSearchType     { typedef Search_T type; };
+
+// When searching const char* from std::string's, especially with linear search there can be a massive performance difference if using const char* instead of std::string or string view, because const char*
+// does not have it's length information readily available, so the comparison can't use size() != size() the determine inequality.
+template <> struct KeySearchTypeToInternalSearchType<std::string, const char*>      { typedef DFG_CLASS_NAME(StringViewC) type; };
+
+// TODO: add specializations to std::wstring & const wchar_t*, typed strings...
+
+// Helper that returns search param as such when there's no specialization of KeySearchTypeToInternalSearchType.
+// TODO: check types, this will e.g. convert non-const ref to const ref.
+template <class Key_T, class Search_T> const Search_T& makeInternalSearchType(const Search_T& t, std::true_type) { return t; }
+
+// Helper that returns specialization as specified by KeySearchTypeToInternalSearchType.
+template <class Key_T, class Search_T>
+typename KeySearchTypeToInternalSearchType<Key_T, Search_T>::type makeInternalSearchType(const Search_T& t, std::false_type)
+{
+    return typename KeySearchTypeToInternalSearchType<Key_T, Search_T>::type(t);
+}
+
+template <class Key_T, class Search_T>
+auto makeInternalSearchType(const Search_T& t) -> typename std::conditional<std::is_same<Search_T, typename KeySearchTypeToInternalSearchType<Key_T, Search_T>::type>::value, const Search_T&, typename KeySearchTypeToInternalSearchType<Key_T, Search_T>::type>::type
+{
+    typedef typename KeySearchTypeToInternalSearchType<Key_T, Search_T>::type InternalSearchType;
+    const auto hasSpecializedSearchType = std::is_same<Search_T, InternalSearchType>();
+    return makeInternalSearchType<Key_T, Search_T>(t, hasSpecializedSearchType);
+}
 
 template <class Traits_T>
 class KeyContainerBase
@@ -48,10 +90,9 @@ public:
     }
 
     template <class This_T, class T>
-    static auto findImpl(This_T& rThis, const T& key) -> decltype(rThis.makeIterator(0)) // iterator // std::conditional<std::is_const<This_T>::value, const_iterator, iterator>::type //, decltype(rThis.makeIterator(0))
+    static auto findImpl(This_T& rThis, const T& keyRaw) -> decltype(rThis.makeIterator(0)) // iterator // std::conditional<std::is_const<This_T>::value, const_iterator, iterator>::type //, decltype(rThis.makeIterator(0))
     {
-        // TODO: implement conversion from T to search optimized type. For example finding with const char* from std::string keys can be slower than needed (especially in linear search) because equality comparison is between 
-        //       std::string and naked const char*. Instead should have a search key of type string view so that comparison can do a size-aware equality check.
+        const auto& key = makeInternalSearchType<typename This_T::key_type>(keyRaw);
         if (rThis.isSorted())
         {
             auto iter = rThis.findInsertPos(rThis, key);
