a3529e7c54706cdfb792d37c479fb837b41ca521
tc3t <tc3t@users.noreply.github.com>
2017-06-07 17:01:12 +0300

    [ref] numeric/accumulate.hpp: tabs -> spaces.

--- dfg/numeric/accumulate.hpp
+++ dfg/numeric/accumulate.hpp
@@ -9,30 +9,30 @@
 
 DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(numeric) {
 
-	namespace DFG_DETAIL_NS
-	{
-		template <class Iterable_T, class Sum_T>
-		Sum_T accumulateImpl(const Iterable_T& iterable, Sum_T sum, std::true_type)
-		{
-			auto p = ptrToContiguousMemory(iterable);
-			const auto nSize = count(iterable);
+    namespace DFG_DETAIL_NS
+    {
+        template <class Iterable_T, class Sum_T>
+        Sum_T accumulateImpl(const Iterable_T& iterable, Sum_T sum, std::true_type)
+        {
+            auto p = ptrToContiguousMemory(iterable);
+            const auto nSize = count(iterable);
             DFG_ZIMPL_VECTORIZING_LOOP(p, nSize, sum += p[i]);
-			return sum;
-		}
+            return sum;
+        }
 
-		template <class Iterable_T, class Sum_T>
-		Sum_T accumulateImpl(const Iterable_T& iterable, Sum_T sum, std::false_type)
-		{
-			return std::accumulate(std::begin(iterable), std::end(iterable), sum);
-		}
-	}
-	
-	template <class Iterable_T, class Sum_T>
-	Sum_T accumulate(const Iterable_T& iterable, Sum_T sum)
-	{
-		typedef typename DFG_ROOT_NS::RangeToBeginPtrOrIter<Iterable_T>::type IterType;
-		return DFG_DETAIL_NS::accumulateImpl(iterable, sum, std::integral_constant<bool, std::is_pointer<IterType>::value>());
-	}
+        template <class Iterable_T, class Sum_T>
+        Sum_T accumulateImpl(const Iterable_T& iterable, Sum_T sum, std::false_type)
+        {
+            return std::accumulate(std::begin(iterable), std::end(iterable), sum);
+        }
+    }
+    
+    template <class Iterable_T, class Sum_T>
+    Sum_T accumulate(const Iterable_T& iterable, Sum_T sum)
+    {
+        typedef typename DFG_ROOT_NS::RangeToBeginPtrOrIter<Iterable_T>::type IterType;
+        return DFG_DETAIL_NS::accumulateImpl(iterable, sum, std::integral_constant<bool, std::is_pointer<IterType>::value>());
+    }
 
     template <class Acc_T, class Iterable_T>
     Acc_T accumulateTyped(const Iterable_T& iterable)
