6e8fe2ed23985b4727b977c0d25d135bf71cad82
tc3t <tc3t@users.noreply.github.com>
2017-06-24 16:58:48 +0300

    [new] io/widePathStrToFstreamFriendlyNonWide.hpp: added pathStrToFileApiFriendlyPath()
    [ci skip]

--- dfg/io/widePathStrToFstreamFriendlyNonWide.hpp
+++ dfg/io/widePathStrToFstreamFriendlyNonWide.hpp
@@ -9,4 +9,12 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(io) {
     // TODO: test
     std::string widePathStrToFstreamFriendlyNonWide(const DFG_CLASS_NAME(ReadOnlySzParamW)& sPath);
 
+    inline DFG_CLASS_NAME(ReadOnlySzParamC) pathStrToFileApiFriendlyPath(const DFG_CLASS_NAME(ReadOnlySzParamC)& sPath) { return sPath; }
+
+#ifdef _WIN32
+    inline DFG_CLASS_NAME(ReadOnlySzParamW) pathStrToFileApiFriendlyPath(const DFG_CLASS_NAME(ReadOnlySzParamW)& sPath) { return sPath; }
+#else
+    inline std::string pathStrToFileApiFriendlyPath(const DFG_CLASS_NAME(ReadOnlySzParamW)& sPath) { return widePathStrToFstreamFriendlyNonWide(sPath); }
+#endif
+
 } } // module namespace
