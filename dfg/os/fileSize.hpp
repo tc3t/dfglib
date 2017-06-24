d17259fbc2198f983c606bc3af184a04332bc4cc
tc3t <tc3t@users.noreply.github.com>
2017-06-24 17:14:38 +0300

    [mod/imp] os/fileSize.hpp: fileSize() is now implemented with stat() instead of open stream & tellg()

--- dfg/os/fileSize.hpp
+++ dfg/os/fileSize.hpp
@@ -2,21 +2,41 @@
 
 #include "../dfgDefs.hpp"
 #include "../dfgBaseTypedefs.hpp"
-#include "../io/BasicIfStream.hpp"
+#include "../io/widePathStrToFstreamFriendlyNonWide.hpp"
 
+#include <sys/types.h>
+#include <sys/stat.h>
 
 DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(os) {
+
+    namespace DFG_DETAIL_NS
+    {
+        template <class Stat_T, class Path_T, class Func_T>
+        std::pair<int, Stat_T> statImpl(const Path_T& path, Func_T func)
+        {
+            Stat_T buffer;
+            auto rv = func(path.c_str(), &buffer);
+            return std::make_pair(rv, buffer);
+        }
+
+#ifdef _WIN32
+        inline std::pair<int, struct __stat64> stat(const DFG_CLASS_NAME(ReadOnlySzParamC)& path) { return statImpl<struct __stat64>(path, _stat64); }
+        inline std::pair<int, struct __stat64> stat(const DFG_CLASS_NAME(ReadOnlySzParamW)& path) { return statImpl<struct __stat64>(path, _wstat64); }
+#else
+        inline std::pair<int, struct stat64> stat(const DFG_CLASS_NAME(ReadOnlySzParamC)& path)
+        { 
+            return statImpl<struct stat64>(path, stat64);
+        }
+#endif
+    }
 
 // TODO: Revise return type.
 template <class Char_T>
 inline auto fileSizeT(const DFG_CLASS_NAME(ReadOnlySzParam)<Char_T>& sFile) -> uint64
 {
-    using namespace DFG_MODULE_NS(io);
-    DFG_CLASS_NAME(BasicIfStream) istrm(sFile);
-    istrm.seekg(DFG_CLASS_NAME(BasicIfStream)::SeekOriginEnd, 0);
-    const auto endPos = istrm.tellg();
-    const uint64 nSize = endPos;
-    return nSize;
+    auto info = DFG_DETAIL_NS::stat(DFG_MODULE_NS(io)::pathStrToFileApiFriendlyPath(sFile));
+    DFG_STATIC_ASSERT(sizeof(info.second.st_size) >= 8, "stat() should use at least 64-bit size.");
+    return (info.first == 0) ? info.second.st_size : 0;
 }
 
 // TODO: Revise return value type.
