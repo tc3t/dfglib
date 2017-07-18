3a24810d6888d5904f1b11c45c5e70022eaa4338
tc3t <tc3t@users.noreply.github.com>
2017-07-19 00:00:07 +0300

    [new] buid/inlineTools.hpp: DFG_FORCEINLINE
    [ci skip]

---
+++ dfg/build/inlineTools.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+/*
+    DFG_FORCEINLINE
+        -Request for the compiler to try to inline a function, a stronger request than 'inline'.
+
+    Note: Does not guarantee that function is actually inlined.
+    Note: There may be different semantics depending on compiler: for example in VC2015 DFG_FORCEINLINE will not be effective on default debug options, but in GCC it may do the inlining in non-optimizated builds.
+*/
+
+#if defined(_MSC_VER)
+    #define DFG_FORCEINLINE    __forceinline
+#elif defined(__GNUC__)
+    #define DFG_FORCEINLINE    __attribute__((always_inline)) inline
+#else
+    #define DFG_FORCEINLINE    inline // TODO: implement
+#endif
