18ad3610e834f34fe0a5eeaaf8f30fe3dbc5eb10
tc3t <tc3t@users.noreply.github.com>
2019-03-24 17:33:05 +0200

    [examples] dfgQtTableEditor: added stubs for About-box.
    -Now shows a "click to see information about the application"-button in TableEditor toolbar.

--- dfgExamples/dfgQtTableEditor/main.cpp
+++ dfgExamples/dfgQtTableEditor/main.cpp
@@ -1,12 +1,23 @@
 #include <dfg/qt/qtIncludeHelpers.hpp>
+#include <dfg/qt/connectHelper.hpp>
 
 DFG_BEGIN_INCLUDE_QT_HEADERS
 #include <QApplication>
+#include <QMessageBox>
 #include <QTimer>
+#include <QStyle>
+#include <QToolButton>
 DFG_END_INCLUDE_QT_HEADERS
 
 #include <dfg/qt/TableEditor.hpp>
 #include <dfg/qt/QtApplication.hpp>
+
+static void onShowAboutBox()
+{
+    QMessageBox::information(nullptr,
+                             QApplication::tr("About dfgQtTableEditor"),
+                             QApplication::tr("TODO"));
+}
 
 int main(int argc, char *argv[])
 {
@@ -27,5 +38,18 @@ int main(int argc, char *argv[])
             tableEditor.tryOpenFileFromPath(args[1]);
         #endif
     }
+
+    // Add about-button to TableEditor toolbar.
+    {
+        auto button = new QToolButton(&tableEditor);
+        button->setToolTip(QApplication::tr("Information about the application"));
+        auto pStyle = a.style();
+        if (pStyle)
+            button->setIcon(pStyle->standardIcon(QStyle::SP_MessageBoxInformation));
+        DFG_QT_VERIFY_CONNECT(QObject::connect(button, &QToolButton::clicked, &onShowAboutBox));
+        tableEditor.addToolBarSeparator();
+        tableEditor.addToolBarWidget(button);
+    }
+
     return a.exec();
 }
