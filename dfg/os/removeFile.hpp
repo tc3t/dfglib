4afc1f34585feb933bb71a77e834371b1795fc93
tc3t <tc3t@users.noreply.github.com>
2019-05-01 23:20:35 +0300

    [imp] os/removeFile: wchar_t version is now available also on non-Windows.

--- dfg/os/removeFile.hpp
+++ dfg/os/removeFile.hpp
@@ -3,12 +3,18 @@
 #include "../dfgDefs.hpp"
 #include <cstdio>
 
+#ifndef _WIN32
+    #include "../io/widePathStrToFstreamFriendlyNonWide.hpp"
+#endif
+
 DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(os)
 {
     // Returns 0 in success, otherwise non-zero (for details see documentation of std::remove())
     inline int removeFile(const char* psz) { return std::remove(psz); }
 #ifdef _WIN32
     inline int removeFile(const wchar_t* psz) { return _wremove(psz); }
+#else
+    inline int removeFile(const wchar_t* psz) { return removeFile(DFG_MODULE_NS(io)::pathStrToFileApiFriendlyPath(psz).c_str()); }
 #endif
 
 } } // module namespace
