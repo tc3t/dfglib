tc3t/dfglib

commit 1a63736fc4d723854cdb36a526e74b9cf6f54024

[fix] os::TemporaryFileStream: fix to path separator handling for non-Windows
-Path construction used \ as separator also on non-Windows. Didn't, however, seem to cause problem when folder path was already ended with separator.

dfg/os/TemporaryFileStream.hpp
@@ -14,6 +14,14 @@
 
 DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(os)
 {
+    DFG_CONSTEXPR char nativeSeparatorChar()
+    {
+#ifdef _WIN32
+        return '\\';
+#else
+        return '/';
+#endif
+    }
         namespace DFG_DETAIL_NS
         {
             // Implement opening stream through open()-method for object of type other than std::ofstream.
@@ -106,8 +114,8 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(os)
                         sFilename += pPszExtensionWithOrWithoutDot;
                     }
                     StringT sPath = (pPszFolderPath) ? pPszFolderPath : tempFolderPath();
-                    if (!sPath.empty() && sPath.back() != '\\')
-                        sPath += '\\';
+                    if (!sPath.empty() && sPath.back() != nativeSeparatorChar())
+                        sPath.push_back(nativeSeparatorChar());
                     sPath += sFilename;
 
                     DFG_DETAIL_NS::openStream(m_ostrm, sPath);

commit d75e16f370886839cf272635b5bdaf8e7ab1a220

[buildfix] os::TemporaryFileStream: broken in 1a63736fc4d723854cdb36a526e74b9cf6f54024

dfg/os/TemporaryFileStream.hpp
@@ -14,7 +14,7 @@
 
 DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(os)
 {
-    DFG_CONSTEXPR char nativeSeparatorChar()
+    inline DFG_CONSTEXPR char nativeSeparatorChar()
     {
 #ifdef _WIN32
         return '\\';