2af6d008b8fc7c0b625f53b6988bd0187ae4f916
tc3t <tc3t@users.noreply.github.com>
2019-06-22 23:16:54 +0300

    [examples] dfgQtTableEditor: added helper scripts for creating binary release package.

---
+++ dfgExamples/dfgQtTableEditor/winqtdeploy.bat
@@ -0,0 +1,99 @@
+@REM Helper script for creating a binary distribution package of dfgQtTableEditor
+@REM Usage:
+@REM
+@REM	Takes three parameters and creates release packages to .7z files.
+@REM
+@REM	Parameters
+@REM		0: QT bin dir path
+@REM		1: dfgQtTableEditor.exe path
+@REM		2: MSVC runtime REDIST dir path
+@REM
+
+@echo off
+
+set QT_BIN_DIR=%1
+set EXE_PATH=%2
+set MSVC_RUNTIME_REDIST_PATH=%3
+
+@echo QT_BIN_DIR is "%QT_BIN_DIR%"
+@echo EXE_PATH is "%EXE_PATH%"
+@echo MSVC_RUNTIME_REDIST_PATH is %MSVC_RUNTIME_REDIST_PATH%
+
+@echo --------------------------------
+@echo Checking input parameters
+if not "%QT_BIN_DIR%" NEQ "" goto :usage
+if not "%EXE_PATH%" NEQ "" goto :usage
+
+set OUTPUT_WORKDIR=winqtdeploy_sandbox
+set PACKAGE_FOLDER_NAME=dfgQtTableEditor
+set PACKAGE_OUTPUT_PATH=%OUTPUT_WORKDIR%\%PACKAGE_FOLDER_NAME%
+
+@echo Output workdir is %OUTPUT_WORKDIR%
+@echo Package output path is %PACKAGE_OUTPUT_PATH%
+
+@echo --------------------------------
+@echo Removing old workdir
+rd /S %OUTPUT_WORKDIR%
+
+@echo --------------------------------
+@echo Running windeployqt with output path %PACKAGE_OUTPUT_PATH%
+
+@rem Notes on windeployqt
+@rem     --no-compiler-runtime is specified as it does not copy runtime dll's but vc_redist.x64.exe installer.
+@rem     Others are mostly to avoid package bloat from unneeded stuff.
+%QT_BIN_DIR%\windeployqt -dir %PACKAGE_OUTPUT_PATH% -no-translations -no-system-d3d-compiler -no-opengl-sw --no-compiler-runtime %EXE_PATH%
+
+if "%errorlevel%" NEQ "0" (
+	@echo windeployqt failed; terminating package creation.
+	exit /b 1
+)
+
+@echo --------------------------------
+@echo Copying exe-file to output dir
+copy %EXE_PATH% %PACKAGE_OUTPUT_PATH%
+
+@echo --------------------------------
+@echo Copying MSVC runtime DLL's
+copy %MSVC_RUNTIME_REDIST_PATH%\msvcp140.dll %PACKAGE_OUTPUT_PATH%
+copy %MSVC_RUNTIME_REDIST_PATH%\msvcp140_1.dll %PACKAGE_OUTPUT_PATH%
+copy %MSVC_RUNTIME_REDIST_PATH%\msvcp140_2.dll %PACKAGE_OUTPUT_PATH%
+copy %MSVC_RUNTIME_REDIST_PATH%\vcruntime140.dll %PACKAGE_OUTPUT_PATH%
+
+@echo --------------------------------
+@echo Removing some unneeded plugins
+del %PACKAGE_OUTPUT_PATH%\imageformats\qgif.dll
+del %PACKAGE_OUTPUT_PATH%\imageformats\qjpeg.dll
+del %PACKAGE_OUTPUT_PATH%\imageformats\qtiff.dll
+del %PACKAGE_OUTPUT_PATH%\imageformats\qwebp.dll
+
+@echo --------------------------------
+@echo Extracting version info from executable
+set EXE_VERSION_FILE_PATH=%OUTPUT_WORKDIR%\exe_version.txt
+powershell "(Get-Item -path %PACKAGE_OUTPUT_PATH%\dfgQtTableEditor.exe).VersionInfo.ProductVersion" > %EXE_VERSION_FILE_PATH%
+set /P EXE_VERSION= < %EXE_VERSION_FILE_PATH%
+del %EXE_VERSION_FILE_PATH%
+@echo Exe version is %EXE_VERSION%
+
+@echo --------------------------------
+@echo Creating .7z packages
+set PACKAGE_PATH_FULL=%OUTPUT_WORKDIR%\dfgQtTableEditor_%EXE_VERSION%_full.7z
+set PACKAGE_PATH_UDPATE=%OUTPUT_WORKDIR%\dfgQtTableEditor_%EXE_VERSION%_update.7z
+cd %OUTPUT_WORKDIR%
+"C:\Program Files\7-Zip\7z.exe" a -mx9 ..\%PACKAGE_PATH_FULL% %PACKAGE_FOLDER_NAME%
+cd ..
+"C:\Program Files\7-Zip\7z.exe" a -mx9 %PACKAGE_PATH_UDPATE% %EXE_PATH%
+
+@echo --------------------------------
+@echo Creating hash files for the packages.
+powershell "(Get-FileHash %PACKAGE_PATH_FULL%).Hash" > %PACKAGE_PATH_FULL%.sha256
+powershell "(Get-FileHash %PACKAGE_PATH_UDPATE%).Hash" > %PACKAGE_PATH_UDPATE%.sha256
+
+@echo --------------------------------
+@echo All done.
+
+goto :exit
+
+:usage
+	@echo Not all expected parameters were given.
+
+:exit
---
+++ dfgExamples/dfgQtTableEditor/winqtdeploy_VC2017.bat
@@ -0,0 +1,13 @@
+@rem Helper script for creating release package from VC2017 build.
+
+@echo off
+
+@rem set_deploy_env.bat is machine-specific file that is expected to set the following variables:
+@rem QT_BIN_DIR, value <Qt install path>\<Qt version>\<compiler folder, e.g. msvc2017_64>\bin\
+@rem MSVC_REDIST_PATH_2017, value e.g. <VS 2017 install dir>\VC\Redist\MSVC\14.16.27012\x64\Microsoft.VC141.CRT\
+@rem VCINSTALLDIR_2017, value <VS 2017 install dir>\VC
+call set_deploy_env.bat
+
+set EXE_PATH=TODO_set_exe_path_here.exe
+set VCINSTALLDIR=%VCINSTALLDIR_2017%
+call winqtdeploy.bat %QT_BIN_DIR% %EXE_PATH% "%MSVC_REDIST_PATH_2017%"
