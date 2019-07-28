5c705ab64912d1fb71cef5b69c5d3b2dbb29f1bd
tc3t <tc3t@users.noreply.github.com>
2019-07-28 16:03:28 +0300

    [imp] Added detection macros for VC2019.2.

--- dfg/preprocessor/compilerInfoMsvc.hpp
+++ dfg/preprocessor/compilerInfoMsvc.hpp
@@ -49,6 +49,9 @@
 #define DFG_MSVC_VER_VC16_1     1921
 #define DFG_MSVC_VER_2019_1     DFG_MSVC_VER_VC16_1
 
+#define DFG_MSVC_VER_VC16_2     1922
+#define DFG_MSVC_VER_2019_2     DFG_MSVC_VER_VC16_2
+
 #if defined(_MSC_VER)
     #define DFG_MSVC_VER	_MSC_VER
 #else
