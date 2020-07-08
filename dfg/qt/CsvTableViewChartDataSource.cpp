tc3t/dfglib

commit fd608077c41bc47ed44cc57bca0d7e119e57b7b2

[imp/mod/reg] qt::graphTools: revamped handling of selection data source
-Previously every selection was cached completely (if there were enabled chart entries using the selection source). Now data is read directly from model when requested so avoids lots of redundant storing and some edit-blocks due to held read-lock. As a regression the time previously spent in another thread parsing the content is now done in UI thread as a blocking operation.

dfg/qt/CsvTableViewChartDataSource.cpp
@@ -25,7 +25,7 @@ void ::DFG_MODULE_NS(qt)::SelectionAnalyzerForGraphing::setChartDefinitionViewer
 void ::DFG_MODULE_NS(qt)::SelectionAnalyzerForGraphing::analyzeImpl(const QItemSelection selection)
 {
     auto pView = qobject_cast<CsvTableView*>(this->m_spView.data());
-    if (!pView || !m_spTable)
+    if (!pView || !m_spSelectionInfo)
         return;
 
     ChartDefinitionViewer* pViewer = m_apChartDefinitionViewer;
@@ -35,82 +35,36 @@ void ::DFG_MODULE_NS(qt)::SelectionAnalyzerForGraphing::analyzeImpl(const QItemS
 
     bool bSignalChange = true;
 
-    m_spTable.edit([&](Table& rTable, const Table* pOld)
-    {
-        // TODO: this may be storing way too much data: e.g. if all items are selected, graphing may not use any or perhaps only two columns.
-        //       -> filter according to actual needs. Note that in order to be able to do so, would need to know something about graph definitions here.
-        // TODO: which indexes to use e.g. in case of filtered table: source row indexes or view rows?
-        //      -e.g. if table of 1000 rows is filtered so that only rows 100, 600 and 700 are shown, should single column graph use
-        //       values 1, 2, 3 or 100, 600, 700 as x-coordinate value? This might be something to define in graph definition -> another reason why
-        //       graph definitions should be somehow known at this point.
 
+
+    m_spSelectionInfo.edit([&](SelectionInfo& rSelectionInfo, const SelectionInfo* pOld)
+    {
         DFG_UNUSED(pOld);
+
+        rSelectionInfo.m_columnNames.clear();
 
         bool bErrorEntries = false;
         if (spChartDefinitionView && !spChartDefinitionView->isSourceUsed(m_sSourceId, &bErrorEntries))
         {
-            rTable.clear();
+            rSelectionInfo.clear();
             if (!bErrorEntries) // If there are no error entries, considering this source as definitely not used so not signaling changes.
                 bSignalChange = false;
             return;
         }
 
-        ::DFG_MODULE_NS(cont)::SetVector<int> presentColumnIndexes;
-        presentColumnIndexes.setSorting(true); // Sorting is needed for std::set_difference
+        static_cast<QItemSelection&>(rSelectionInfo) = selection;
 
-        // Clearing all values from every column so that those would actually contain current values, not remnants from previous update.
-        // Note that removing unused columns is done after going through the selection.
-        ::DFG_MODULE_NS(alg)::forEachFwd(rTable.valueRange(), [](RowToValueMap& columnValues) { columnValues.clear(); });
-
-        // Sorting is needed for std::set_difference
-        rTable.setSorting(true);
-
-        CompletionStatus completionStatus = CompletionStatus_started;
-        rTable.m_columnTypes.clear();
-        rTable.m_columnNames.clear();
-
-        // Getting column names
+        // Getting column names from columns present in selection.
         {
             auto pModel = pView->csvModel();
             if (pModel)
             {
-                const auto nCount = pModel->columnCount();
-                for (int i = 0; i < nCount; ++i)
-                    rTable.m_columnNames[i] = pModel->getHeaderName(i);
+                for (const auto& item : rSelectionInfo)
+                {
+                    for (int i = item.left(), right = item.right(); i <= right; ++i)
+                        rSelectionInfo.m_columnNames[i] = pModel->getHeaderName(i);
+                }
             }
-        }
-
-        CsvTableView::forEachIndexInSelection(*pView, selection, CsvTableView::ModelIndexTypeView, [&](const QModelIndex& mi, bool& rbContinue)
-        {
-            if (m_abNewSelectionPending)
-            {
-                completionStatus = ::DFG_MODULE_NS(qt)::DFG_CLASS_NAME(CsvTableViewSelectionAnalyzer)::CompletionStatus_terminatedByNewSelection;
-                rbContinue = false;
-                return;
-            }
-            else if (!m_abIsEnabled.load(std::memory_order_relaxed))
-            {
-                completionStatus = ::DFG_MODULE_NS(qt)::DFG_CLASS_NAME(CsvTableViewSelectionAnalyzer)::CompletionStatus_terminatedByDisabling;
-                rbContinue = false;
-                return;
-            }
-
-            if (!mi.isValid())
-                return;
-            auto sVal = mi.data().toString();
-            presentColumnIndexes.insert(mi.column());
-            // Note that indexes are view indexes, not source model indexes (e.g. in case of filtered table, row indexes in filtered table)
-            const auto nCol = mi.column();
-            rTable[mi.column()][CsvItemModel::internalRowIndexToVisible(mi.row())] = GraphDataSource::cellStringToDouble(sVal, nCol, rTable.m_columnTypes);
-        });
-
-        // Removing unused columns from rTable.
-        std::vector<int> colsToRemove;
-        const auto colsInTable = rTable.keyRange();
-        std::set_difference(colsInTable.cbegin(), colsInTable.cend(), presentColumnIndexes.cbegin(), presentColumnIndexes.cend(), std::back_inserter(colsToRemove));
-        for (auto iter = colsToRemove.cbegin(), iterEnd = colsToRemove.cend(); iter != iterEnd; ++iter)
-        {
-            rTable.erase(*iter);
         }
     });
 
@@ -125,10 +79,10 @@ void ::DFG_MODULE_NS(qt)::SelectionAnalyzerForGraphing::analyzeImpl(const QItemS
         return;
     m_uniqueId = QString("viewSelection_%1").arg(QString::number(QDateTime::currentMSecsSinceEpoch() % 1000000));
     m_spSelectionAnalyzer = std::make_shared<SelectionAnalyzerForGraphing>();
-    m_spSelectionAnalyzer->m_spTable.reset(std::make_shared<SelectionAnalyzerForGraphing::Table>());
+    m_spSelectionAnalyzer->m_spSelectionInfo.reset(std::make_shared<SelectionAnalyzerForGraphing::SelectionInfo>());
     m_spSelectionAnalyzer->m_sSourceId = this->uniqueId();
-    m_dataViewer = m_spSelectionAnalyzer->m_spTable.createViewer();
-    DFG_QT_VERIFY_CONNECT(connect(m_spSelectionAnalyzer.get(), &CsvTableViewSelectionAnalyzer::sigAnalyzeCompleted, this, &GraphDataSource::sigChanged));
+    m_selectionViewer = m_spSelectionAnalyzer->m_spSelectionInfo.createViewer();
+    DFG_QT_VERIFY_CONNECT(connect(m_spSelectionAnalyzer.get(), &CsvTableViewSelectionAnalyzer::sigAnalyzeCompleted, this, &CsvTableViewChartDataSource::onSelectionAnalysisCompleted));
     this->m_bAreChangesSignaled = true;
     m_spView->addSelectionAnalyzer(m_spSelectionAnalyzer);
 }
@@ -143,87 +97,94 @@ QObject* ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::underlyingSource()
     return m_spView;
 }
 
-void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::forEachElement_byColumn(DataSourceIndex c, ForEachElementByColumHandler handler)
+void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::forEachElement_byColumn(DataSourceIndex nColRaw, ForEachElementByColumHandler handler)
 {
-    if (!handler || !m_spView)
+    if (!handler || !m_spView || !m_spView)
         return;
 
-    auto spTableView = m_dataViewer.view();
-    if (!spTableView) // This may happen e.g. if the table is being updated in analyzeImpl() (in another thread). 
-        return;
-    const auto& rTable = *spTableView;
-
-    auto iter = rTable.find(c);
-
-    if (iter == rTable.end())
+    auto pModel = m_spView->model();
+    if (!pModel)
         return;
 
-    const auto& columnData = iter->second;
+    auto spSelectionViewer = this->privGetSelectionViewer();
+    if (!spSelectionViewer) // This may happen e.g. if the table is being updated in analyzeImpl() (in another thread). 
+        return;
 
-    handler(columnData.m_keyStorage.data(), columnData.m_valueStorage.data(), nullptr, columnData.size());
+    auto editLockReleaser = m_spView->tryLockForEdit();
+    if (!editLockReleaser.isLocked())
+        return;
+
+    const QItemSelection& selection  = *spSelectionViewer;
+
+    const auto c = saturateCast<int>(nColRaw);
+
+    for (const auto& item : selection)
+    {
+        if (item.left() > c || item.right() < c)
+            continue;
+        for (int r = item.top(), rBottom = item.bottom(); r <= rBottom; ++r)
+        {
+            auto data = pModel->data(pModel->index(r, c));
+            if (data.isNull())
+                return;
+            auto sVal = data.toString();
+            // Note that indexes are view indexes, not source model indexes (e.g. in case of filtered table, row indexes in filtered table)
+            const double x = CsvItemModel::internalRowIndexToVisible(r);
+            const double y = GraphDataSource::cellStringToDouble(sVal, c, m_columnTypes);
+            handler(&x, &y, nullptr, 1);
+        }
+    }
 }
 
 auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::columnIndexes() const -> IndexList
 {
     IndexList rv;
-    auto spTableView = privGetTableView();
-    if (spTableView)
-        rv.assign(spTableView->keyRange());
+    auto spSelectionViewer = privGetSelectionViewer();
+    if (spSelectionViewer)
+        std::transform(spSelectionViewer->m_columnNames.begin(), spSelectionViewer->m_columnNames.end(), std::back_inserter(rv), [](const auto& keyVal) { return keyVal.first; });
     return rv;
 }
 
 auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::columnCount() const -> DataSourceIndex
 {
-    auto spTableView = privGetTableView();
-    return (spTableView) ? spTableView->size() : 0;
+    auto spSelectionViewer = privGetSelectionViewer();
+    return (spSelectionViewer) ? spSelectionViewer->m_columnNames.size() : 0;
 }
 
 auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::columnIndexByName(const StringViewUtf8 sv) const -> DataSourceIndex
 {
-    auto pModel = (m_spView) ? m_spView->csvModel() : nullptr;
-    if (!pModel)
+    auto spSelectionViewer = privGetSelectionViewer();
+    if (!spSelectionViewer)
         return invalidIndex();
-    auto nIndex = pModel->findColumnIndexByName(QString::fromUtf8(sv.beginRaw(), static_cast<int>(sv.size())), -1);
-    if (nIndex < 0)
-        return invalidIndex();
-    // Getting here means that column was found from the CsvItemModel table; must still check if the column is present in selection.
-    auto spTableView = privGetTableView();
-    auto pTable = (spTableView) ? &*spTableView : nullptr;
-    if (!pTable)
-        return invalidIndex();
-    auto iter = pTable->find(nIndex);
-    return (iter != pTable->end()) ? static_cast<DataSourceIndex>(iter->first) : invalidIndex();
+    const auto sSearch = viewToQString(sv);
+    // Linear search from values of (index, column name) map
+    for (const auto& kv : spSelectionViewer->m_columnNames)
+    {
+        if (kv.second == sSearch)
+            return kv.first;
+    }
+    return invalidIndex();
 }
 
 auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::singleColumnDoubleValues_byOffsetFromFirst(const DataSourceIndex offsetFromFirst) -> SingleColumnDoubleValuesOptional
 {
-    auto spTableView = privGetTableView();
-    if (!spTableView) // This may happen e.g. if the table is being updated in analyzeImpl() (in another thread). 
+    auto spSelectionViewer = privGetSelectionViewer();
+    if (!spSelectionViewer) // This may happen e.g. if the selection is being updated in analyzeImpl() (in another thread). 
         return SingleColumnDoubleValuesOptional();
-    const auto& rTable = *spTableView;
-    if (rTable.empty())
+    if (offsetFromFirst < 0 || offsetFromFirst >= spSelectionViewer->m_columnNames.size())
         return SingleColumnDoubleValuesOptional();
-    const DataSourceIndex nFirstCol = static_cast<DataSourceIndex>(rTable.frontKey());
-    const auto nTargetCol = nFirstCol + offsetFromFirst;
-    return singleColumnDoubleValues_byColumnIndex(nTargetCol);
+    return singleColumnDoubleValues_byColumnIndex((spSelectionViewer->m_columnNames.begin() + offsetFromFirst)->first);
 }
 
 auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::singleColumnDoubleValues_byColumnIndex(const DataSourceIndex nColIndex) -> SingleColumnDoubleValuesOptional
 {
-    auto spTableView = privGetTableView();
-    if (!spTableView) // This may happen e.g. if the table is being updated in analyzeImpl() (in another thread). 
-        return SingleColumnDoubleValuesOptional();
-    const auto& rTable = *spTableView;
-    if (rTable.empty())
-        return SingleColumnDoubleValuesOptional();
-    auto iter = rTable.find(nColIndex);
-    if (iter == rTable.end())
-        return SingleColumnDoubleValuesOptional();
-    const auto& values = iter->second.valueRange();
     auto rv = std::make_shared<DoubleValueVector>();
-    rv->resize(values.size());
-    std::copy(values.cbegin(), values.cend(), rv->begin());
-    return std::move(rv); // explicit move to avoid "call 'std::move' explicitly to avoid copying on older compilers"-warning in Qt Creator
+    forEachElement_byColumn(nColIndex, [&](const double* pX, const double* pY, Dummy, const DataSourceIndex n)
+    {
+        DFG_UNUSED(pX);
+        rv->insert(rv->end(), pY, pY + n);
+    });
+    return std::move(rv); // explicit move to avoid "call 'std::move' explicitly to avoid copying on older compilers"-warning in Qt Creator   
 }
 
 void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::enable(const bool b)
@@ -241,23 +202,28 @@ void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::enable(const bool b)
 
 auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::columnDataTypes() const -> ColumnDataTypeMap
 {
-    auto spViewer = privGetTableView();
-    return (spViewer) ? spViewer->m_columnTypes : ColumnDataTypeMap();
+    return m_columnTypes;
 }
 
 auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::columnNames() const -> ColumnNameMap
 {
-    auto spViewer = privGetTableView();
-    return (spViewer) ? spViewer->m_columnNames : ColumnNameMap();
+    auto spSelectionViewer = privGetSelectionViewer();
+    return (spSelectionViewer) ? spSelectionViewer->m_columnNames : ColumnNameMap();
 }
 
-auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::privGetTableView() const -> std::shared_ptr<const SelectionAnalyzerForGraphing::Table>
+auto ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::privGetSelectionViewer() const -> std::shared_ptr<const SelectionAnalyzerForGraphing::SelectionInfo>
 {
-    return m_dataViewer.view();
+    return m_selectionViewer.view();
 }
 
 void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::setChartDefinitionViewer(ChartDefinitionViewer sp)
 {
     if (m_spSelectionAnalyzer)
         m_spSelectionAnalyzer->setChartDefinitionViewer(std::move(sp));
+}
+
+void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::onSelectionAnalysisCompleted()
+{
+    m_columnTypes.clear();
+    Q_EMIT sigChanged();
 }

dfg/qt/CsvTableView.cpp
@@ -3531,6 +3531,17 @@ void DFG_CLASS_NAME(CsvTableView)::privShowExecutionBlockedNotification(const QS
     QToolTip::showText(QCursor::pos(), privCreateActionBlockedDueToLockedContentMessage(actionname));
 }
 
+auto DFG_CLASS_NAME(CsvTableView)::tryLockForEdit() -> LockReleaser
+{
+    return (m_spEditLock && m_spEditLock->tryLockForWrite()) ? LockReleaser(m_spEditLock.get()) : LockReleaser();
+}
+
+DFG_CLASS_NAME(CsvTableView)::LockReleaser::~LockReleaser()
+{
+    if (m_pLock)
+        m_pLock->unlock();
+}
+
 DFG_MODULE_NS(qt)::DFG_CLASS_NAME(CsvTableViewSelectionAnalyzer)::DFG_CLASS_NAME(CsvTableViewSelectionAnalyzer)()
     : m_abIsEnabled(true)
     , m_bPendingCheckQueue(false)

dfg/qt/CsvTableView.hpp
@@ -151,6 +151,19 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
             ModelIndexTypeView    // Refers to indexes in view model (which can be the same as source-model in case there is no proxy).
         };
 
+        class LockReleaser
+        {
+        public:
+            LockReleaser(QReadWriteLock* pLock = nullptr) : m_pLock(pLock) {}
+            LockReleaser(LockReleaser&& other) : m_pLock(other.m_pLock) { other.m_pLock = nullptr; }
+            ~LockReleaser();
+            LockReleaser(const LockReleaser&) = delete;
+            LockReleaser& operator=(const LockReleaser&) = delete;
+            bool isLocked() { return m_pLock != nullptr; }
+
+            QReadWriteLock* m_pLock;
+        };
+
         DFG_CLASS_NAME(CsvTableView)(QWidget* pParent);
         ~DFG_CLASS_NAME(CsvTableView)() DFG_OVERRIDE_DESTRUCTOR;
 
@@ -259,6 +272,8 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
 
         // Convenience function, effectively returns selectionModel()->selection();
         QItemSelection getSelection() const;
+
+        LockReleaser tryLockForEdit();
 
     private:
         template <class T, class Param0_T>

commit 3676049174a4e2e037840131ab8bbfcbdf2e12e1

[fix] qt::CsvTableViewChartDataSource: empty lines no longer terminate graph data
-Broken in fd608077

dfg/qt/CsvTableViewChartDataSource.cpp
@@ -125,8 +125,6 @@ void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::forEachElement_byColumn(D
         for (int r = item.top(), rBottom = item.bottom(); r <= rBottom; ++r)
         {
             auto data = pModel->data(pModel->index(r, c));
-            if (data.isNull())
-                return;
             auto sVal = data.toString();
             // Note that indexes are view indexes, not source model indexes (e.g. in case of filtered table, row indexes in filtered table)
             const double x = CsvItemModel::internalRowIndexToVisible(r);