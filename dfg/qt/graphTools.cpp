tc3t/dfglib

commit 8c5dfcb746a82be1434873e3f7e8f394ac85cbc2

[imp/mod/ref] qt::graphTools: Minor changes to date/time representations
-Axis tick labels will now be shown with less details if appropriate: for example if all datetimes have time part 00:00:00, axis tick labels will show only date part. Also if milliseconds part is always .000, tick labels will omit milliseconds in labels.
-Previously column data type was determined by last value; now the most precise format in the data is used.

dfg/qt/graphTools.cpp
@@ -291,10 +291,29 @@ double ::DFG_MODULE_NS(qt)::GraphDataSource::stringToDouble(const QString& s)
 // Return value in case of invalid input as GIGO, in most cases returns NaN. Also in case of invalid input typeMap's value at nCol is unspecified.
 double ::DFG_MODULE_NS(qt)::GraphDataSource::cellStringToDouble(const QString& s, const DataSourceIndex nCol, ColumnDataTypeMap& typeMap)
 {
-    const auto setColumnDataType = [&](const ChartDataType t)
+    const auto updateColumnDataType = [&](ChartDataType t)
     {
-        typeMap[nCol] = t;
+        auto insertRv = typeMap.insert(nCol, t);
+        if (!insertRv.second) // Already existed?
+            insertRv.first->second.setIfExpands(t);
     };
+    const auto dateToDoubleAndColumnTypeHandling = [&](QDateTime&& dt, ChartDataType dataTypeHint)
+    {
+        const auto& time = dt.time();
+        if (dataTypeHint.isDateNoTzType() && time.msecsSinceStartOfDay() == 0 && dataTypeHint != ChartDataType::dateOnlyYearMonth)
+            dataTypeHint = ChartDataType::dateOnly;
+        else if (time.msec() == 0)
+        {
+            if (dataTypeHint == ChartDataType::dateAndTimeMillisecond)
+                dataTypeHint = ChartDataType::dateAndTime;
+            else if (dataTypeHint == ChartDataType::dateAndTimeMillisecondTz)
+                dataTypeHint = ChartDataType::dateAndTimeTz;
+        }
+        updateColumnDataType(dataTypeHint);
+        return dateToDouble(std::move(dt));
+    };
+
+
     const auto isTzStartChar = [](const QChar& c) { return ::DFG_MODULE_NS(alg)::contains("Z+-", c.toLatin1()); };
     // TODO: add parsing for fractional part longer than 3 digits.
     if (s.size() >= 8 && s[4] == '-' && s[7] == '-') // Something starting with ????-??-?? (ISO 8601, https://en.wikipedia.org/wiki/ISO_8601)
@@ -307,70 +326,46 @@ double ::DFG_MODULE_NS(qt)::GraphDataSource::cellStringToDouble(const QString& s
             {
                 // Timezone specifier after milliseconds?
                 if (s.size() >= 24 && isTzStartChar(s[23]))
-                {
-                    setColumnDataType(ChartDataType::dateAndTimeMillisecondTz);
-                    return dateToDouble(QDateTime::fromString(s, Qt::ISODateWithMs));
-                }
+                    return dateToDoubleAndColumnTypeHandling(QDateTime::fromString(s, Qt::ISODateWithMs), ChartDataType::dateAndTimeMillisecondTz);
                 else
-                {
-                    setColumnDataType(ChartDataType::dateAndTimeMillisecond);
-                    return dateToDouble(QDateTime::fromString(s, QString("yyyy-MM-dd%1hh:mm:ss.zzz").arg(s[10])));
-                }
+                    return dateToDoubleAndColumnTypeHandling(QDateTime::fromString(s, QString("yyyy-MM-dd%1hh:mm:ss.zzz").arg(s[10])), ChartDataType::dateAndTimeMillisecond);
             }
             else if (s.size() >= 20 && isTzStartChar(s[19]))
-            {
-                setColumnDataType(ChartDataType::dateAndTimeTz);
-                return dateToDouble(QDateTime::fromString(s, Qt::ISODate));
-            }
+                return dateToDoubleAndColumnTypeHandling(QDateTime::fromString(s, Qt::ISODate), ChartDataType::dateAndTimeTz);
             else
-            {
-                setColumnDataType(ChartDataType::dateAndTime);
-                return dateToDouble(QDateTime::fromString(s, QString("yyyy-MM-dd%1hh:mm:ss").arg(s[10])));
-            }
+                return dateToDoubleAndColumnTypeHandling(QDateTime::fromString(s, QString("yyyy-MM-dd%1hh:mm:ss").arg(s[10])), ChartDataType::dateAndTime);
         }
         else if (s.size() == 13 && s[10] == ' ') // Case: "yyyy-MM-dd Wd". where Wd is two char weekday indicator.
-        {
-            setColumnDataType(ChartDataType::dateOnly);
-            return dateToDouble(QDateTime::fromString(s, QString("yyyy-MM-dd'%1'").arg(s.mid(10, 3))));
-        }
+            return dateToDoubleAndColumnTypeHandling(QDateTime::fromString(s, QString("yyyy-MM-dd'%1'").arg(s.mid(10, 3))), ChartDataType::dateOnly);
         else if (s.size() == 10) // Case: "yyyy-MM-dd"
-        {
-            setColumnDataType(ChartDataType::dateOnly);
-            return dateToDouble(QDateTime::fromString(s, "yyyy-MM-dd"));
-        }
+            return dateToDoubleAndColumnTypeHandling(QDateTime::fromString(s, "yyyy-MM-dd"), ChartDataType::dateOnly);
         else
-        {
             return std::numeric_limits<double>::quiet_NaN();
-        }
     }
 
     // yyyy-mm
     if (s.size() == 7 && s[4] == '-')
-    {
-        setColumnDataType(ChartDataType::dateOnlyYearMonth);
-        return dateToDouble(QDateTime::fromString(s, QString("yyyy-MM")));
-    }
+        return dateToDoubleAndColumnTypeHandling(QDateTime::fromString(s, "yyyy-MM"), ChartDataType::dateOnlyYearMonth);
 
     // [d]d.[m]m.yyyy
     QRegExp regExp(R"((?:^|^\w\w )(\d{1,2})(?:\.)(\d{1,2})(?:\.)(\d\d\d\d)$)");
     if (regExp.exactMatch(s) && regExp.captureCount() == 3)
     {
-        setColumnDataType(ChartDataType::dateOnly);
         const auto items = regExp.capturedTexts();
         // 0 has entire match, so actual captures start from index 1.
-        return dateToDouble(QDateTime(QDate(regExp.cap(3).toInt(), regExp.cap(2).toInt(), regExp.cap(1).toInt())));
+        return dateToDoubleAndColumnTypeHandling(QDateTime(QDate(regExp.cap(3).toInt(), regExp.cap(2).toInt(), regExp.cap(1).toInt())), ChartDataType::dateOnly);
     }
 
     if (s.size() >= 8 && s[2] == ':' && s[5] == ':')
     {
         if (s.size() >= 10 && s[8] == '.')
         {
-            setColumnDataType(ChartDataType::dayTimeMillisecond);
+            updateColumnDataType(ChartDataType::dayTimeMillisecond);
             return timeToDouble(QTime::fromString(s, "hh:mm:ss.zzz"));
         }
         else
         {
-            setColumnDataType(ChartDataType::dayTime);
+            updateColumnDataType(ChartDataType::dayTime);
             return timeToDouble(QTime::fromString(s, "hh:mm:ss"));
         }
     }

dfg/charts/commonChartTools.hpp
@@ -111,18 +111,84 @@ constexpr char ChartObjectFieldIdStr_lineColour[] = "line_colour";
 
 } // namespace fieldsIds
 
-enum class ChartDataType
+// Enum-like class for defining chart data type and for related queries.
+class ChartDataType
 {
-    unknown,
-    dayTime,
-    dayTimeMillisecond,
-    dateOnly,
-    dateOnlyYearMonth,
-    dateAndTime,
-    dateAndTimeTz,
-    dateAndTimeMillisecond,
-    dateAndTimeMillisecondTz
+public:
+    enum DataType
+    {
+        unknown,
+        // dayTime-domain
+        dayTime,
+        dayTimeMillisecond,
+        firstDayTimeItem = dayTime,
+        lastDayTimeItem = dayTimeMillisecond,
+        // Date without timezone domain
+        dateOnlyYearMonth,
+        dateOnly,
+        dateAndTime,
+        dateAndTimeMillisecond,
+        firstDateNoTzItem = dateOnlyYearMonth,
+        lastDateNoTzItem = dateAndTimeMillisecond,
+        // Date with timezone domaiin
+        dateAndTimeTz,
+        dateAndTimeMillisecondTz,
+        firstDateWithTzItem = dateAndTimeTz,
+        lastDateWithTzItem = dateAndTimeMillisecondTz
+    };
+
+    ChartDataType(DataType dt = unknown) :
+        m_dataType(dt)
+    {}
+
+    operator DataType() const
+    {
+        return m_dataType;
+    }
+
+    bool isDateNoTzType() const
+    {
+        return m_dataType >= firstDateNoTzItem && m_dataType <= lastDateNoTzItem;
+    }
+
+    bool isDateWithTzType() const
+    {
+        return m_dataType >= firstDateWithTzItem && m_dataType <= lastDateWithTzItem;
+    }
+
+    bool isTimeType() const
+    {
+        return m_dataType >= firstDayTimeItem && m_dataType <= lastDayTimeItem;
+    }
+
+    // Sets new type if both are of the same domain and arg expands type in the domain; e.g. dateOnly -> dateAndTime is expanding, but not the other way around.
+    void setIfExpands(const ChartDataType other);
+
+    DataType m_dataType;
 };
+
+// Sets new type if both are of the same domain and arg expands type in the domain; e.g. dateOnly -> dateAndTime is expanding, but not the other way around.
+inline void ChartDataType::setIfExpands(const ChartDataType other)
+{
+    if (m_dataType == unknown)
+        return;
+    else if (this->isTimeType())
+    {
+        if (other.isTimeType())
+            m_dataType = Max(m_dataType, other.m_dataType);
+    }
+    else if (this->isDateNoTzType())
+    {
+        if (other.isDateNoTzType())
+            m_dataType = Max(m_dataType, other.m_dataType);
+    }
+    else if (this->isDateWithTzType())
+    {
+        if (other.isDateWithTzType())
+            m_dataType = Max(m_dataType, other.m_dataType);
+    }
+}
+
 
 // Abstract class representing an entry that defines what kind of ChartObjects to create on chart.
 class AbstractChartControlItem

commit 62d093478d2c8eae9181f54f436ab1075e6a25ed

[buildfix] qt::graphTools: Broken in 8c5dfcb7 at least for non-MSVC compilers

dfg/qt/graphTools.cpp
@@ -759,7 +759,7 @@ auto DFG_MODULE_NS(qt)::TableSelectionCacheItem::columnDataType(const RowToValue
 {
     const auto nCol = columnToIndex(pColumn);
     auto iter = m_columnTypes.find(nCol);
-    return (iter != m_columnTypes.end()) ? iter->second : ChartDataType::unknown;
+    return (iter != m_columnTypes.end()) ? iter->second : ChartDataType(ChartDataType::unknown);
 }
 
 auto DFG_MODULE_NS(qt)::TableSelectionCacheItem::columnName(const RowToValueMap* pColumn) const -> QString
@@ -3211,8 +3211,8 @@ void DFG_MODULE_NS(qt)::GraphControlAndDisplayWidget::refreshXy(ChartCanvas& rCh
     const bool bYisRowIndex = rowFlags[1];
 
     const char szRowIndexName[] = QT_TR_NOOP("Row number");
-    const auto xType = (!bXisRowIndex) ? tableData.columnDataType(pXdata) : ChartDataType::unknown;
-    const auto yType = (!bYisRowIndex) ? tableData.columnDataType(pYdata) : ChartDataType::unknown;
+    const auto xType = (!bXisRowIndex) ? tableData.columnDataType(pXdata) : ChartDataType(ChartDataType::unknown);
+    const auto yType = (!bYisRowIndex) ? tableData.columnDataType(pYdata) : ChartDataType(ChartDataType::unknown);
     const auto sXname = (!bXisRowIndex) ? tableData.columnName(pXdata) : QString(szRowIndexName);
     const auto sYname = (!bYisRowIndex) ? tableData.columnName(pYdata) : QString(szRowIndexName);
     auto spSeries = rChart.getSeriesByIndex_createIfNonExistent(XySeriesCreationParam(nGraphCounter++, configParamCreator(), defEntry, xType, yType, qStringToStringUtf8(sXname), qStringToStringUtf8(sYname)));