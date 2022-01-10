tc3t/dfglib

commit 3197a325d75924a2ad9354f091e77b291244c2f0

[ref] various tweaks to silence compiler and static analyzer warnings (from MSVC and Qt Creator analyzers)

dfg/str/strCat.hpp
@@ -46,8 +46,8 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(str) {
     auto strCat(Str_T&& dest, const PARAMTYPE& s0) -> DFG_TEMP_RETURN_TYPE \
     { \
         PARAMTYPE arrParams[] = { s0 }; \
-        DFG_DETAIL_NS::strCatImpl(std::forward<Str_T>(dest), arrParams, DFG_COUNTOF(arrParams)); \
-        return std::forward<Str_T>(dest); \
+        DFG_DETAIL_NS::strCatImpl(dest, arrParams, DFG_COUNTOF(arrParams)); \
+        return dest; \
     } \
     template <class Str_T> \
     auto strCat(Str_T&& dest, const PARAMTYPE& s0, const PARAMTYPE& s1) -> DFG_TEMP_RETURN_TYPE \

dfg/charts/operations.hpp
@@ -429,7 +429,8 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(charts) {
             error_unexpectedInputVectorCount    = 0x10,   // Input has unexpected vector count.
             error_unexpectedInputVectorTypes    = 0x20,   // Input has unexpected type (e.g. string instead of number)
             error_unableToCreateDataVectors     = 0x40,   // Operation failed because new data vectors couldn't be created.
-            error_processingError               = 0x80    // Error occured while operating, this can happen for example if creation args are bad and/or incompatible with input.
+            error_processingError               = 0x80,   // Error occured while operating, this can happen for example if creation args are bad and/or incompatible with input.
+            error_unableToDefineVariables       = 0x100   // Failed to define variables to internal parser.
         };
         using ErrorMask = ::DFG_MODULE_NS(cont)::Flags<Error>;
 
@@ -983,10 +984,11 @@ namespace operations
             return;
         }
 
-        if (pValuesX)
-            parser.defineVariable("x", &x);
-        if (pValuesY)
-            parser.defineVariable("y", &y);
+        if ((pValuesX && !parser.defineVariable("x", &x)) || (pValuesY && !parser.defineVariable("y", &y)))
+        {
+            op.setError(error_unableToDefineVariables);
+            return;
+        }
 
         const auto nSize = (pValuesX) ? pValuesX->size() : pValuesY->size();
         if (pValuesX && pValuesY && pValuesX->size() != pValuesY->size())

dfg/crypt.hpp
@@ -2,6 +2,7 @@
 
 #include "dfgDefs.hpp"
 #include "dfgBase.hpp" // For DFG_COUNTOF
+#include <type_traits>
 
 #ifdef _WIN32
     #include <stdexcept>
@@ -95,7 +96,7 @@ DFG_ROOT_NS_BEGIN { DFG_SUB_NS(crypt) {
         bool Generate(T& obj)
         //-------------------------
         {
-            static_assert(std::is_pod<T>::value == true, "Generate is available only for PODs");
+            DFG_STATIC_ASSERT(std::is_trivially_copyable<T>::value == true, "Generate is available only for trivially copyable T");
             return Generate(&obj, sizeof(obj));
         }
 

dfg/dfgBase.hpp
@@ -11,6 +11,7 @@
 #include "scopedCaller.hpp"
 #include "rangeIterator.hpp"
 #include <cmath>
+#include <type_traits>
 
 #include "dfgBaseTypedefs.hpp"
 #include "bits/byteSwap.hpp"
@@ -181,7 +182,7 @@ inline void limitMax(T& val, const C& upperLimit)
 //       (To be sure that the memory gets zeroed, call memsetZeroSecure)
 template <class T> inline void memsetZero(T& a)
 {
-    static_assert(std::is_pod<T>::value == true && std::is_pointer<T>::value == false, "Won't memset non-pods or pointers");
+    DFG_STATIC_ASSERT(std::is_trivially_copyable<T>::value == true && std::is_pointer<T>::value == false, "Won't memset pointers or types that are not trivially copyable");
     memset(&a, 0, sizeof(a));
 }
 

dfg/io/readBinary.hpp
@@ -22,10 +22,7 @@ template <class StrmT> StrmT& readBinary(StrmT& istrm, void* pBuf, const size_t 
 template <class Strm_T, class T>
 Strm_T& readBinary(Strm_T& istrm, T& obj)
 {
-#ifdef _MSC_VER // TODO: Add proper check, this was added for GCC 4.8.1
-    // is_pod was added for VC2013, where, unlike with VC2010 and VC2012, array of pods returned false for has_trivial_assign<T>.
-    DFG_STATIC_ASSERT(DFG_MODULE_NS(TypeTraits)::IsTriviallyCopyAssignable<T>::value == true || std::is_pod<T>::value == true, "readBinary(Strm_T, T&) only accepts trivially assignable or pod T's");
-#endif
+    DFG_STATIC_ASSERT(std::is_trivially_copyable<T>::value == true, "readBinary(Strm_T, T&) only accepts trivially copyable T");
     return readBinary(istrm, &obj, sizeof(obj));
 }
 

dfg/math/FormulaParser.cpp
@@ -459,8 +459,10 @@ auto ::DFG_MODULE_NS(math)::FormulaParser::setFormula(const StringViewC sv) -> R
 
 double ::DFG_MODULE_NS(math)::FormulaParser::setFormulaAndEvaluateAsDouble(const StringViewC sv)
 {
-    setFormula(sv);
-    return this->evaluateFormulaAsDouble();
+    if (setFormula(sv))
+        return this->evaluateFormulaAsDouble();
+    else
+        return std::numeric_limits<double>::quiet_NaN();
 }
 
 auto ::DFG_MODULE_NS(math)::FormulaParser::defineVariable(const StringViewC sv, double* pVar) -> ReturnStatus

dfg/memsetZeroSecure.hpp
@@ -31,7 +31,7 @@ inline void memsetZeroSecure(const VoidPtr pData, const size_t nSize)
 template <class T>
 inline void memsetZeroSecure(T& a)
 {
-	static_assert(std::is_pod<T>::value == true && std::is_pointer<T>::value == false, "Won't memset non-pods or pointers");
+	DFG_STATIC_ASSERT(std::is_trivially_copyable<T>::value == true && std::is_pointer<T>::value == false, "Won't memset pointers or types that are not trivially copyable");
 	memsetZeroSecure(&a, sizeof(a));
 }
 

dfg/qt/CsvTableView.cpp
@@ -517,15 +517,15 @@ namespace
         auto pProgressDialog = new ProgressWidget(sProgressDialogLabel, isCancellable, pParent);
         auto pWorkerThread = new QThread();
         pWorkerThread->setObjectName(sThreadName); // Sets thread name visible to debugger.
-        QObject::connect(pWorkerThread, &QThread::started, [&]()
+        DFG_QT_VERIFY_CONNECT(QObject::connect(pWorkerThread, &QThread::started, [&]()
                 {
                     func(pProgressDialog);
                     pWorkerThread->quit();
-                });
+                }));
         // Connect thread finish to trigger event loop quit and closing of progress bar.
-        QObject::connect(pWorkerThread, &QThread::finished, &eventLoop, &QEventLoop::quit);
-        QObject::connect(pWorkerThread, &QThread::finished, pWorkerThread, &QObject::deleteLater);
-        QObject::connect(pWorkerThread, &QObject::destroyed, pProgressDialog, &QObject::deleteLater);
+        DFG_QT_VERIFY_CONNECT(QObject::connect(pWorkerThread, &QThread::finished, &eventLoop, &QEventLoop::quit));
+        DFG_QT_VERIFY_CONNECT(QObject::connect(pWorkerThread, &QThread::finished, pWorkerThread, &QObject::deleteLater));
+        DFG_QT_VERIFY_CONNECT(QObject::connect(pWorkerThread, &QObject::destroyed, pProgressDialog, &QObject::deleteLater));
 
         pWorkerThread->start();
 
@@ -3074,7 +3074,8 @@ namespace
             else if (genType == GeneratorTypeFormula)
             {
                 ::DFG_MODULE_NS(math)::FormulaParser parser;
-                parser.defineRandomFunctions();
+                if (!parser.defineRandomFunctions())
+                    QMessageBox::information(this, tr("Internal error"), tr("Internal error occurred causing documentation of random functions to be unavailable"));
                 QString sFuncNames;
                 decltype(sFuncNames.length()) nLastBrPos = 0;
                 parser.forEachDefinedFunctionNameWhile([&](const ::DFG_ROOT_NS::StringViewC& sv)
@@ -3809,18 +3810,26 @@ bool CsvTableView::generateContentImpl(const CsvItemModel& settingsModel)
         const auto sFormula = settingsModel.data(settingsModel.index(LastNonParamPropertyId + 1, 1)).toString().toUtf8();
         ::DFG_MODULE_NS(math)::FormulaParser parser;
         DFG_VERIFY(parser.defineRandomFunctions());
-        parser.setFormula(sFormula.data());
+        if (!parser.setFormula(sFormula.data()))
+        {
+            DFG_ASSERT_WITH_MSG(false, "Failed to set formula to parser");
+            return false;
+        }
         char buffer[32] = "";
         double tr = std::numeric_limits<double>::quiet_NaN();
         double tc = std::numeric_limits<double>::quiet_NaN();
         const double rowCount = rModel.rowCount();
         const double colCount = rModel.columnCount();
         CsvItemModel::DataTable* pTable = nullptr;
-        parser.defineFunctor("cellValue", [&](double r, double c) { return cellValueAsDouble(pTable, r, c); }, false);
-        parser.defineVariable("trow", &tr);
-        parser.defineVariable("tcol", &tc);
-        parser.defineConstant("rowcount", rowCount);
-        parser.defineConstant("colcount", colCount);
+        if (!parser.defineFunctor("cellValue", [&](double r, double c) { return cellValueAsDouble(pTable, r, c); }, false)
+            || !parser.defineVariable("trow", &tr)
+            || !parser.defineVariable("tcol", &tc)
+            || !parser.defineConstant("rowcount", rowCount)
+            || !parser.defineConstant("colcount", colCount))
+        {
+            DFG_ASSERT_WITH_MSG(false, "Failed to set functions/variables/constants to parser");
+            return false;
+        }
         const auto formatType = adjustFormatAndGetType(pszFormat);
         const auto generator = [&](CsvItemModel::DataTable& table, const int r, const int c, size_t)
         {
@@ -6014,7 +6023,7 @@ ProgressWidget::ProgressWidget(const QString sLabelText, const IsCancellable isC
     if (isCancellable == IsCancellable::yes)
     {
         auto spCancelButton = std::unique_ptr<QPushButton>(new QPushButton(tr("Cancel"), this));
-        connect(spCancelButton.get(), &QPushButton::clicked, [&]() { m_abCancelled = true; });
+        DFG_QT_VERIFY_CONNECT(connect(spCancelButton.get(), &QPushButton::clicked, [&]() { m_abCancelled = true; }));
         setCancelButton(spCancelButton.release()); // "The progress dialog takes ownership"
     }
     else

commit 19178fe60c5bd1967021bef95079c9d8ec702c35

[imp] str::strCat: no longer creates redundant copy when given rvalue destination
-Partly broken in 3197a325d75924a2ad9354f091e77b291244c2f0

dfg/str/strCat.hpp
@@ -39,6 +39,7 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(str) {
 
     // Defines return type: Str_T for rvalues and Str_T& for lvalues.
 #define DFG_TEMP_RETURN_TYPE typename std::conditional<std::is_rvalue_reference<Str_T&&>::value, Str_T, Str_T&>::type
+#define DFG_TEMP_STRCAT_RETURN return std::forward<Str_T>(dest);
 
     // Defines the actual functions for given char type. These are just boilerplate interfaces passing the actual work to strCatImpl().
 #define DFG_TEMP_MACRO_DEFINE_STRCAT(PARAMTYPE, STR) \
@@ -47,35 +48,35 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(str) {
     { \
         PARAMTYPE arrParams[] = { s0 }; \
         DFG_DETAIL_NS::strCatImpl(dest, arrParams, DFG_COUNTOF(arrParams)); \
-        return dest; \
+        DFG_TEMP_STRCAT_RETURN \
     } \
     template <class Str_T> \
     auto strCat(Str_T&& dest, const PARAMTYPE& s0, const PARAMTYPE& s1) -> DFG_TEMP_RETURN_TYPE \
     { \
         PARAMTYPE arrParams[] = { s0, s1 }; \
         DFG_DETAIL_NS::strCatImpl(dest, arrParams, DFG_COUNTOF(arrParams)); \
-        return dest; \
+        DFG_TEMP_STRCAT_RETURN \
     } \
     template <class Str_T> \
     auto strCat(Str_T&& dest, const PARAMTYPE& s0, const PARAMTYPE& s1, const PARAMTYPE& s2) -> DFG_TEMP_RETURN_TYPE \
     { \
         PARAMTYPE arrParams[] = { s0, s1, s2 }; \
         DFG_DETAIL_NS::strCatImpl(dest, arrParams, DFG_COUNTOF(arrParams)); \
-        return dest; \
+        DFG_TEMP_STRCAT_RETURN \
     } \
     template <class Str_T> \
     auto strCat(Str_T&& dest, const PARAMTYPE& s0, const PARAMTYPE& s1, const PARAMTYPE& s2, const PARAMTYPE& s3) -> DFG_TEMP_RETURN_TYPE \
     { \
         PARAMTYPE arrParams[] = { s0, s1, s2, s3 }; \
         DFG_DETAIL_NS::strCatImpl(dest, arrParams, DFG_COUNTOF(arrParams)); \
-        return dest; \
+        DFG_TEMP_STRCAT_RETURN \
     } \
     template <class Str_T> \
     auto strCat(Str_T&& dest, const PARAMTYPE& s0, const PARAMTYPE& s1, const PARAMTYPE& s2, const PARAMTYPE& s3, const PARAMTYPE& s4) -> DFG_TEMP_RETURN_TYPE \
     { \
         PARAMTYPE arrParams[] = { s0, s1, s2, s3, s4 }; \
         DFG_DETAIL_NS::strCatImpl(dest, arrParams, DFG_COUNTOF(arrParams)); \
-        return dest; \
+        DFG_TEMP_STRCAT_RETURN \
     } \
     template <class Str_T> \
     auto strCat(Str_T&& dest, const PARAMTYPE& s0, const PARAMTYPE& s1, const PARAMTYPE& s2, const PARAMTYPE& s3, const PARAMTYPE& s4, const PARAMTYPE& s5, \
@@ -84,7 +85,7 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(str) {
     { \
         PARAMTYPE arrParams[] = { s0, s1, s2, s3, s4, s5, s6, s7, s8, s9 }; \
         DFG_DETAIL_NS::strCatImpl(dest, arrParams, DFG_COUNTOF(arrParams)); \
-        return dest; \
+        DFG_TEMP_STRCAT_RETURN \
     } 
 
     // Define strCat for char wchar_t parameters.
@@ -96,5 +97,6 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(str) {
 
 #undef DFG_TEMP_MACRO_DEFINE_STRCAT
 #undef DFG_TEMP_RETURN_TYPE
+#undef DFG_TEMP_STRCAT_RETURN
 
 } } // module namespace

dfgTest/dfgTestStr.cpp
@@ -293,6 +293,24 @@ TEST(dfgStr, strCat)
         strCat(sCapacityTest, "");
         EXPECT_EQ(nCapacityAfterReserve, sCapacityTest.capacity());
     }
+
+    // Checking that storage of rvalue input string is not duplicated
+    {
+        std::string sSource;
+        const auto stringReturner = [&]() -> std::string&& { sSource = "12345679801234567890"; return std::move(sSource); };
+
+        auto sCat = strCat(stringReturner(), "a");
+        DFGTEST_EXPECT_LEFT("12345679801234567890a", sCat);
+        DFGTEST_EXPECT_TRUE(sSource.empty());
+
+        sCat = strCat(stringReturner(), "a", "b");
+        DFGTEST_EXPECT_LEFT("12345679801234567890ab", sCat);
+        DFGTEST_EXPECT_TRUE(sSource.empty());
+
+        std::string sSource2 = "abc";
+        strCat(sSource2, "d", "e");
+        DFGTEST_EXPECT_LEFT("abcde", sSource2);
+    }
 }
 
 TEST(dfgStr, strToByLexCast)