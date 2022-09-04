tc3t/dfglib

commit c6eef5830cf804caad119e110f196d375262e32e

[ref] qt::CsvTableView: added lock-usage to column property accesses

dfg/qt/CsvTableView.cpp
@@ -4860,7 +4860,7 @@ auto CsvTableView::tryLockForEdit() -> LockReleaser
     return (m_spEditLock && m_spEditLock->tryLockForWrite()) ? LockReleaser(m_spEditLock.get()) : LockReleaser();
 }
 
-auto CsvTableView::tryLockForRead() -> LockReleaser
+auto CsvTableView::tryLockForRead() const -> LockReleaser
 {
     return (m_spEditLock && m_spEditLock->tryLockForRead()) ? LockReleaser(m_spEditLock.get()) : LockReleaser();
 }
@@ -4996,16 +4996,27 @@ namespace
     }
 } // unnamed namespace
 
+template <class Func_T>
+QVariant CsvTableView::getColumnPropertyByDataModelIndexImpl(int nDataModelCol, QVariant defaultValue, Func_T func) const
+{
+    // This is brittle: if getting read lock fails, this function may effectively return bogus column property which in turn
+    // can mean e.g. that UI shows faulty property status for a column. getColInfo() should probably always guarantee somekind of access
+    // to 'latest known' column info.
+    auto lockReleaser = tryLockForRead();
+    if (!lockReleaser.isLocked())
+        return defaultValue;
+    auto pColInfo = getColInfo(*this, nDataModelCol);
+    return (pColInfo) ? func(*pColInfo) : defaultValue;
+}
+
 QVariant CsvTableView::getColumnPropertyByDataModelIndex(const int nCol, const StringViewUtf8& svKey, const QVariant defaultValue) const
 {
-    auto pColInfo = getColInfo(*this, nCol);
-    return (pColInfo) ? getColumnProperty(*this, *pColInfo, svKey, defaultValue) : defaultValue;
+    return getColumnPropertyByDataModelIndexImpl(nCol, defaultValue, [&](const CsvItemModel::ColInfo& rColInfo) { return getColumnProperty(*this, rColInfo, svKey, defaultValue); });
 }
 
 QVariant CsvTableView::getColumnPropertyByDataModelIndex(const int nCol, const CsvItemModelColumnProperty propertyId, const QVariant defaultValue) const
 {
-    auto pColInfo = getColInfo(*this, nCol);
-    return (pColInfo) ? pColInfo->getProperty(propertyId, defaultValue) : defaultValue;
+    return getColumnPropertyByDataModelIndexImpl(nCol, defaultValue, [&](const CsvItemModel::ColInfo& rColInfo) { return rColInfo.getProperty(propertyId, defaultValue); });
 }
 
 void CsvTableView::setCaseSensitiveSorting(const bool bCaseSensitive)

dfg/qt/CsvTableView.hpp
@@ -435,7 +435,7 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
         QItemSelection getSelection() const;
 
         LockReleaser tryLockForEdit(); // Note: edits include both content edit and view edits such as changing filter.
-        LockReleaser tryLockForRead();
+        LockReleaser tryLockForRead() const;
 
         TableHeaderView* horizontalTableHeader();
 
@@ -671,6 +671,9 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
         QString askConfigFilePath(CsvItemModel& rModel);
         bool saveConfigFileTo(const CsvConfig& config, const QString& sPath);
 
+        template <class Func_T>
+        QVariant getColumnPropertyByDataModelIndexImpl(int nDataModelCol, QVariant defaultValue, Func_T func) const;
+
     public:
         std::unique_ptr<DFG_MODULE_NS(cont)::TorRef<QUndoStack>> m_spUndoStack;
         QStringList m_tempFilePathsToRemoveOnExit;
@@ -681,7 +684,7 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
         std::unique_ptr<QMenu> m_spResizeColumnsMenu;
         bool m_bUndoEnabled;
         std::vector<QObjectStorage<QThread>> m_analyzerThreads;
-        std::shared_ptr<QReadWriteLock> m_spEditLock; // For controlling when table can be edited.
+        mutable std::shared_ptr<QReadWriteLock> m_spEditLock; // For controlling when table can be edited.
         DFG_OPAQUE_PTR_DECLARE();
     }; // class CsvTableView
 

commit 0045b1d4a06371f90e5e5472942b576cd61410ad

[fix] qt::CsvTableView: Column visibility operations broke in c6eef583
-For example unticking "Visible" for a column would not hide the column from UI

dfg/qt/CsvTableView.cpp
@@ -5067,7 +5067,10 @@ void CsvTableView::setColumnVisibility(const int nCol, const bool bVisible, cons
         return;
 
     if (pColInfo->setProperty(viewPropertyContextId(*this), ColumnPropertyId::visible, bVisible) && proxyInvalidation == ProxyModelInvalidation::ifNeeded)
+    {
+        lockReleaser.unlock(); // Must be unlocked before invalidating since UI update may malfunction if holding edit-lock.
         invalidateSortFilterProxyModel();
+    }
 }
 
 auto CsvTableView::getCellEditability(const RowIndex_data nRow, const ColumnIndex_data nCol) const -> CellEditability
@@ -5134,6 +5137,7 @@ void CsvTableView::unhideAllColumns()
         return true;
     });
 
+    lockReleaser.unlock(); // Must be unlocked before invalidating since UI update may malfunction if holding edit-lock.
     invalidateSortFilterProxyModel();
 }
 
@@ -5185,6 +5189,7 @@ void CsvTableView::showSelectColumnVisibilityDialog()
     {
         this->setColumnVisibility(c, visibilityFlags[c], ProxyModelInvalidation::no);
     }
+    lockReleaser.unlock(); // Must be unlocked before invalidating since UI update may malfunction if holding edit-lock.
     invalidateSortFilterProxyModel();
 }
 

dfg/qt/containerUtils.hpp
@@ -123,6 +123,7 @@ public:
     LockReleaser& operator=(LockReleaser&& other) noexcept;
     LockReleaser& operator=(const LockReleaser&) = delete;
     bool isLocked() { return m_pLock != nullptr; }
+    void unlock();
 
     QReadWriteLock* m_pLock;
 }; // class LockReleaser
@@ -140,6 +141,11 @@ inline auto LockReleaser::operator=(LockReleaser&& other) noexcept -> LockReleas
     this->m_pLock = other.m_pLock;
     other.m_pLock = nullptr;
     return *this;
+}
+
+inline void LockReleaser::unlock()
+{
+    *this = LockReleaser();
 }
 
 }} // Module namespace