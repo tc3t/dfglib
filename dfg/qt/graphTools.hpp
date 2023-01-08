tc3t/dfglib

commit a039fe9a8f88d7a74d1829dfd85eac1d9d8b4b5e

[imp/ref] qt::CsvTableViewChartDataSource: row data is no longer cached
-Also includes work on SourceDataSpan to allow providing rows as value range instead of memory span.

dfg/qt/graphTools.hpp
@@ -10,7 +10,9 @@
 #include "../cont/ViewableSharedPtr.hpp"
 #include "../charts/commonChartTools.hpp"
 #include "../charts/operations.hpp"
+#include "../iter/FunctionValueIterator.hpp"
 #include "../OpaquePtr.hpp"
+#include "../rangeIterator.hpp"
 #include <memory>
 #include <vector>
 #include <functional>
@@ -75,11 +77,81 @@ public:
 using ChartDefinitionViewable = ::DFG_MODULE_NS(cont)::ViewableSharedPtr<ChartDefinition>;
 using ChartDefinitionViewer = ::DFG_MODULE_NS(cont)::ViewableSharedPtrViewer<ChartDefinition>;
 
+namespace DFG_DETAIL_NS
+{
+    class ColumnDataFeed
+    {
+    public:
+        using ValueIterator = decltype(::DFG_MODULE_NS(iter)::makeIndexIterator(size_t(0)));
+        using ValueRange = RangeIterator_T<ValueIterator>;
+
+        // Copies data to given output range, outputRange must be big enough to receive all elements. If output range is bigger than number of element that needs to be copied, excess output range is left unmodified.
+        template <class Iter_T>
+        void copyDataTo(RangeIterator_T<Iter_T> outputRange) const;
+
+        // Calls given function with data range. Note that given function must accept both Span and ValueRange (e.g. be a generic lambda)
+        template <class Func_T>
+        void doForRange(Func_T func) const;
+
+        bool hasData() const { return m_nSize > 0; }
+        bool hasSpan() const { return !m_span.empty(); }
+        bool hasValueRange() const { return hasData() && !hasSpan(); }
+
+        template <class RowIterable_T>
+        void setData(const RowIterable_T& data);
+
+        void setDataAsIndexSequenceByCountFirst(const size_t nCount, const size_t nFirst);
+
+        Span<const double> asSpan() const { return m_span; }
+        ValueRange asValueRange() const { return makeRange(m_valueIterator, m_valueIterator + m_nSize); }
+
+        size_t m_nSize = 0;
+        ValueIterator m_valueIterator = ::DFG_MODULE_NS(iter)::makeIndexIterator(size_t(0));
+        Span<const double> m_span;
+    }; // class ColumnDataFeed
+
+    template <class RowIterable_T>
+    void ColumnDataFeed::setData(const RowIterable_T& data)
+    {
+        this->m_span = data;
+        this->m_nSize = this->m_span.size();
+    }
+
+    inline void ColumnDataFeed::setDataAsIndexSequenceByCountFirst(const size_t nCount, const size_t nFirst)
+    {
+        m_span = Span<const double>();
+        m_nSize = nCount;
+        m_valueIterator = ::DFG_MODULE_NS(iter)::makeIndexIterator(nFirst);
+    }
+
+
+    template <class Func_T>
+    inline void ColumnDataFeed::doForRange(Func_T func) const
+    {
+        if (this->hasSpan())
+            func(this->asSpan());
+        else
+            func(this->asValueRange());
+    }
+
+    template <class Iter_T>
+    inline void ColumnDataFeed::copyDataTo(RangeIterator_T<Iter_T> outputRange) const
+    {
+        doForRange([&](const auto range)
+            {
+                const auto nSize = Min(outputRange.size(), range.size());
+                std::copy(range.begin(), range.begin() + nSize, outputRange.begin());
+            });
+    }
+
+} // namespace DFG_DETAIL_NS
 
 // Provides a block of data from GraphDataSource, basically a variant of spans of different type.
 class SourceDataSpan
 {
 public:
+    using ColumnDataFeed = ::DFG_MODULE_NS(qt)::DFG_DETAIL_NS::ColumnDataFeed;
+
     template <class T> using SpanT = RangeIterator_T<const T*>;
 
     void clear()
@@ -87,10 +159,17 @@ public:
         std::memset(this, 0, sizeof(*this));
     }
 
+    void setRowsAsIndexSequenceByCountFirst(const size_t nCount, const size_t nFirst)
+    {
+        DFG_ASSERT(m_nSize == 0 || nCount == size());
+        m_rows.setDataAsIndexSequenceByCountFirst(nCount, nFirst);
+    }
+
     template <class RowIterable_T>
     void setRows(const RowIterable_T& rows)
     {
-        privSetSpan(SpanT<double>(makeRange(rows)), m_pRowSpan);
+        DFG_ASSERT(m_nSize == 0 || rows.size() == size());
+        m_rows.setData(rows);
     }
 
     // Sets values
@@ -132,11 +211,10 @@ public:
         return SpanT<T>(pSource, pSource + ((pSource) ? size() : 0));
     }
 
-    SpanT<double>         rows()        const { return privMakeSpan(m_pRowSpan); }
+    const ColumnDataFeed& rows()        const { return m_rows; }
     SpanT<double>         doubles()     const { return privMakeSpan(m_pDoubleSpan); }
     SpanT<StringViewUtf8> stringViews() const { return privMakeSpan(m_pUtf8ViewSpan); }
-
-    const double*         m_pRowSpan      = nullptr; // TODO: should be possible to provide rows with (first, last) indexes.
+    ColumnDataFeed        m_rows;
     const double*         m_pDoubleSpan   = nullptr;
     const QVariant*       m_pVariantSpan  = nullptr;
     const StringViewUtf8* m_pUtf8ViewSpan = nullptr;

dfg/qt/CsvTableViewChartDataSource.cpp
@@ -26,34 +26,23 @@ class CsvTableViewChartDataSource::DataSourceNumberCache
 public:
     using Index = CsvItemModel::Index;
     using NumberContT = ::DFG_MODULE_NS(cont)::ValueVector<double>;
-    using RowContT = NumberContT;
     template <class T> using SharedStorage = std::shared_ptr<T>;
-
-    bool hasRows() const;
 
     bool hasColumn(const ColumnIndex_data col) const;
 
     // Returns new cache object for single column with reserved memory for requested elements, or null-storage if unable to create (e.g. lack of memory)
     SharedStorage<NumberContT> makeNewColumnCacheObject(const Index nReserveCount);
 
-    // Returns true if row cache of requested size exists after the call, false otherwise.
-    bool createRowCacheIfNeeded(const DataSourceIndex nRowCount);
-
     void setColumnCache(const ColumnIndex_data col, SharedStorage<const NumberContT> newData);
-
-    Span<const double> getRowSpan(Index nFirstRow, Index nLastRow) const;
 
     Span<const double> getSpanFromColumn(const ColumnIndex_data col, Index nFirstRow, Index nLastRow) const;
 
     // Returns whole column span
     Span<const double> getSpanFromColumn(const ColumnIndex_data col) const;
         
-    SharedStorage<const RowContT> m_spRows;
     ::DFG_MODULE_NS(cont)::MapVectorSoA<Index, SharedStorage<const NumberContT>> m_mapColToNumbers;
 }; // class CsvTableViewChartDataSource::DataSourceNumberCache
 
-
-bool CsvTableViewChartDataSource::DataSourceNumberCache::hasRows() const { return m_spRows && !m_spRows->empty(); }
 
 bool CsvTableViewChartDataSource::DataSourceNumberCache::hasColumn(const ColumnIndex_data col) const
 {
@@ -76,33 +65,9 @@ auto CsvTableViewChartDataSource::DataSourceNumberCache::makeNewColumnCacheObjec
     }
 }
 
-// Returns true if row cache of requested size exists after the call, false otherwise.
-bool CsvTableViewChartDataSource::DataSourceNumberCache::createRowCacheIfNeeded(const DataSourceIndex nRowCount)
-{
-    if (m_spRows && m_spRows->size() == nRowCount)
-        return true;
-    try
-    {
-        SharedStorage<RowContT> spRows(new RowContT(nRowCount));
-        ::DFG_MODULE_NS(alg)::generateAdjacent(*spRows, Index(CsvItemModel::internalRowIndexToVisible(0)), Index(1));
-        m_spRows = std::move(spRows);
-        return true;
-    }
-    catch (...)
-    {
-        m_spRows.reset();
-        return false;
-    }
-}
-
 void CsvTableViewChartDataSource::DataSourceNumberCache::setColumnCache(const ColumnIndex_data col, SharedStorage<const NumberContT> newData)
 {
     m_mapColToNumbers[col.value()] = std::move(newData);
-}
-
-Span<const double> CsvTableViewChartDataSource::DataSourceNumberCache::getRowSpan(const Index nFirstRow, const Index nLastRow) const
-{
-    return (this->m_spRows) ? Span<const double>(makeRangeFromStartAndEndIndex(*this->m_spRows, nFirstRow, nLastRow + 1)) : Span<const double>();
 }
 
 Span<const double> CsvTableViewChartDataSource::DataSourceNumberCache::getSpanFromColumn(const ColumnIndex_data col, const Index nFirstRow, const Index nLastRow) const
@@ -296,22 +261,18 @@ void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::forEachElement_byColumn(c
         if (selection.size() == 1 && double(selection.front().height()) / double(pCsvModel->rowCount()) > 0.9)
         {
             std::unique_ptr<DataSourceNumberCache> newCache(new DataSourceNumberCache(spCacheView ? *spCacheView : DataSourceNumberCache()));
-
-            if (newCache->createRowCacheIfNeeded(nTotalRowCount)) // TODO: shouldn't need to have (potentially massive) row cache for trivial iota-indexes; would need some work on the data query protocol, though.
+            auto spColumnCache = newCache->makeNewColumnCacheObject(nTotalRowCount);
+            if (spColumnCache)
             {
-                auto spColumnCache = newCache->makeNewColumnCacheObject(nTotalRowCount);
-                if (spColumnCache)
+                for (CsvItemModel::Index r = 0; r < nTotalRowCount; ++r)
                 {
-                    for (CsvItemModel::Index r = 0; r < nTotalRowCount; ++r)
-                    {
-                        const auto pszData = pCsvModel->rawStringPtrAt(r, nColData.value());
-                        const auto val = cellStringToDoubleImpl(pszData, nColView, m_columnTypes);
-                        spColumnCache->push_back(val);
-                    }
-                    newCache->setColumnCache(nColData, std::move(spColumnCache));
-                    DFG_OPAQUE_REF().m_cache.reset(std::move(newCache));
-                    spCacheView = cacheViewer.view();
+                    const auto pszData = pCsvModel->rawStringPtrAt(r, nColData.value());
+                    const auto val = cellStringToDoubleImpl(pszData, nColView, m_columnTypes);
+                    spColumnCache->push_back(val);
                 }
+                newCache->setColumnCache(nColData, std::move(spColumnCache));
+                DFG_OPAQUE_REF().m_cache.reset(std::move(newCache));
+                spCacheView = cacheViewer.view();
             }
         }
     }
@@ -333,8 +294,9 @@ void ::DFG_MODULE_NS(qt)::CsvTableViewChartDataSource::forEachElement_byColumn(c
             SourceDataSpan dataSpan;
             const auto nFirstRow = item.top();
             const auto nLastRow = item.bottom();
+            const auto nRowCount = ::DFG_MODULE_NS(math)::numericDistance(nFirstRow, nLastRow) + 1u;
             if (queryDetails.areRowsRequested())
-                dataSpan.setRows(spCacheView->getRowSpan(nFirstRow, nLastRow));
+                dataSpan.setRowsAsIndexSequenceByCountFirst(nRowCount, CsvItemModel::internalRowIndexToVisible(nFirstRow));
             if (queryDetails.areNumbersRequested())
                 dataSpan.set(spCacheView->getSpanFromColumn(nColData, nFirstRow, nLastRow));
             handler(dataSpan);

dfg/qt/graphTools.cpp
@@ -452,11 +452,11 @@ void ::DFG_MODULE_NS(qt)::GraphDataSource::fetchColumnNumberData(GraphDataSource
         double* pRows = nullptr;
         double* pDoubles = nullptr;
         pipe.getFillBuffers(inputSpan.size(),
-                            (!inputSpan.rows().empty()) ? &pRows : nullptr,
+                            (inputSpan.rows().hasData()) ? &pRows : nullptr,
                             (!inputSpan.doubles().empty()) ? &pDoubles : nullptr
                             );
         if (pRows)
-            std::copy(inputSpan.rows().begin(), inputSpan.rows().end(), pRows);
+            inputSpan.rows().copyDataTo(makeRange(pRows, pRows + inputSpan.size()));
         if (pDoubles)
             std::copy(inputSpan.doubles().begin(), inputSpan.doubles().end(), pDoubles);
     });
@@ -741,6 +741,9 @@ private:
     template <class Map_T, class Inserter_T>
     bool storeColumnFromSourceImpl(Map_T& mapIndexToStorage, GraphDataSource& source, const DataSourceIndex nColumn, const DataQueryDetails& queryDetails, Inserter_T inserter);
 
+    template <class RowRange_T, class ValueRange_T, class ValueConverter_T>
+    void pushBackToRowToStringMap(RowToStringMap& rowToStringMap, const RowRange_T& rowRange, const ValueRange_T& valueRange, ValueConverter_T valueConverter);
+
 public:
 
     ColumnToValuesMap m_colToValuesMap;
@@ -889,39 +892,41 @@ bool DFG_MODULE_NS(qt)::TableSelectionCacheItem::storeColumnFromSource(GraphData
     const auto inserter = [&](RowToValueMap& values, const SourceDataSpan& sourceData)
     {
         const auto& doubleRange = sourceData.doubles();
-        if (sourceData.rows().empty() && !doubleRange.empty())
+        const auto rows = sourceData.rows();
+        if (!rows.hasData() && !doubleRange.empty())
         {
             // If data has doubles but not rows, generating iota rows.
             auto iter = ::DFG_MODULE_NS(iter)::makeFunctionValueIterator(size_t(values.size()), [](size_t i) { return i; });
             values.pushBackToUnsorted(makeRange(iter, iter + doubleRange.size()), doubleRange);
         }
         else
-            values.pushBackToUnsorted(sourceData.rows(), doubleRange);
+            rows.doForRange([&](const auto& range) { values.pushBackToUnsorted(range, doubleRange); });
     };
     
     return storeColumnFromSourceImpl(m_colToValuesMap, source, nColumn, DataQueryDetails(DataQueryDetails::DataMaskRowsAndNumerics), inserter);
+}
+
+template <class RowRange_T, class ValueRange_T, class ValueConverter_T>
+void DFG_MODULE_NS(qt)::TableSelectionCacheItem::pushBackToRowToStringMap(RowToStringMap& rowToStringMap, const RowRange_T& rowRange, const ValueRange_T& valueRange, ValueConverter_T valueConverter)
+{
+    const auto rowIdentityFunc = [](const double row) { return row; };
+    rowToStringMap.pushBackToUnsorted(rowRange, rowIdentityFunc, valueRange, valueConverter);
 }
 
 bool DFG_MODULE_NS(qt)::TableSelectionCacheItem::storeColumnFromSource_strings(GraphDataSource& source, const DataSourceIndex nColumn)
 {
     const auto inserter = [&](RowToStringMap& rowToStringMap, const SourceDataSpan& sourceData)
     {
-        const auto rowRange = sourceData.rows();
         const auto stringViews = sourceData.stringViews();
-        const auto rowIdentityFunc = [](const double row) { return row; };
         if (!stringViews.empty())
         {
-            rowToStringMap.pushBackToUnsorted(rowRange, rowIdentityFunc,
-                                              stringViews,
-                                              [](const StringViewUtf8& sv) { return StringUtf8::fromRawString(sv.beginRaw(), sv.endRaw()); });
+            sourceData.rows().doForRange([&](const auto& rows) { pushBackToRowToStringMap(rowToStringMap, rows, stringViews, [](const StringViewUtf8& sv) { return StringUtf8::fromRawString(sv.beginRaw(), sv.endRaw()); }); });
             return;
         }
         const auto values = sourceData.doubles();
         if (!values.empty())
         {
-            rowToStringMap.pushBackToUnsorted(rowRange, rowIdentityFunc,
-                                              values,
-                                              [](const double d) { return ::DFG_MODULE_NS(str)::floatingPointToStr<StringUtf8>(d); });
+            sourceData.rows().doForRange([&](const auto& rows) { pushBackToRowToStringMap(rowToStringMap, rows, values, [](const double d) { return ::DFG_MODULE_NS(str)::floatingPointToStr<StringUtf8>(d); }); });
             return;
         }
     };

commit a40457f26fb8ab1f0ba290aa18b7666d290e218e

[test/buildfix] dfgTestQt: build fixes, broken in interface changes in a039fe9a

dfg/qt/graphTools.hpp
@@ -97,6 +97,8 @@ namespace DFG_DETAIL_NS
         bool hasSpan() const { return !m_span.empty(); }
         bool hasValueRange() const { return hasData() && !hasSpan(); }
 
+        bool empty() const { return !hasData(); }
+
         template <class RowIterable_T>
         void setData(const RowIterable_T& data);
 

dfgTestQt/dfgTestQt.cpp
@@ -345,7 +345,7 @@ static void testFileDataSource(const QString& sExtension,
         auto& source = *spSource;
         source.forEachElement_byColumn(c, DataQueryDetails(DataQueryDetails::DataMaskAll), [&](const SourceDataSpan& sourceSpan)
         {
-            rows.insert(rows.end(), sourceSpan.rows().begin(), sourceSpan.rows().end());
+            rows.insert(rows.end(), sourceSpan.rows().asSpan().begin(), sourceSpan.rows().asSpan().end());
             const auto nOldSize = static_cast<uint16>(strings.size());
             strings.resize(nOldSize + sourceSpan.stringViews().size());
             std::transform(sourceSpan.stringViews().begin(), sourceSpan.stringViews().end(), strings.begin() + nOldSize, [](const StringViewUtf8& sv) { return sv.toString().rawStorage(); });
@@ -388,7 +388,7 @@ static void testFileDataSource(const QString& sExtension,
         auto& source = *spSource;
         source.forEachElement_byColumn(nCol, DataQueryDetails(mask), [&](const SourceDataSpan& sourceSpan)
         {
-            rows.insert(rows.end(), sourceSpan.rows().begin(), sourceSpan.rows().end());
+            rows.insert(rows.end(), sourceSpan.rows().asSpan().begin(), sourceSpan.rows().asSpan().end());
             const auto nOldSize = static_cast<uint16>(strings.size());
             strings.resize(nOldSize + sourceSpan.stringViews().size());
             std::transform(sourceSpan.stringViews().begin(), sourceSpan.stringViews().end(), strings.begin() + nOldSize, [](const StringViewUtf8& sv) { return sv.toString().rawStorage(); });
@@ -422,7 +422,7 @@ static void testFileDataSource(const QString& sExtension,
         spSource->fetchColumnNumberData(DataPipe(&valueMapFetched), nColumn, queryDetails);
         spSource->forEachElement_byColumn(nColumn, queryDetails, [&](const SourceDataSpan& data)
         {
-            valueMapForEach.pushBackToUnsorted(data.rows(), data.doubles());
+            valueMapForEach.pushBackToUnsorted(data.rows().asSpan(), data.doubles());
         });
         valueMapForEach.setSorting(true);
         EXPECT_EQ(valueMapFetched.m_keyStorage, valueMapForEach.m_keyStorage);