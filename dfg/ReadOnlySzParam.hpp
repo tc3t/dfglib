tc3t/dfglib

commit f75e6e6dd9c7cea97aa5beda66e7c2037fe4c06f

[imp] StringView(Sz): added trimFront()

dfg/ReadOnlySzParam.hpp
@@ -12,6 +12,8 @@
 #include "str/string.hpp"
 #include "build/languageFeatureInfo.hpp"
 #include <string_view>
+#include "alg.hpp"
+#include "Span.hpp"
 
 DFG_ROOT_NS_BEGIN {
 
@@ -182,7 +184,7 @@ protected:
 
 namespace DFG_DETAIL_NS
 {
-    // Base common for both both view and viewSz. Most of the the stuff in StringView and StringViewSz should be here.
+    // Base common for both both view and viewSz. Most of the implementation logics of StringView and StringViewSz should preferably be here.
     template <class Str_T>
     class StringViewCommonBase
     {
@@ -234,6 +236,25 @@ namespace DFG_DETAIL_NS
         std::basic_string_view<CharT> toStdStringView(const size_t nSize) const
         {
             return std::basic_string_view<CharT>(this->dataRaw(), nSize);
+        }
+
+        // Removes every base character from front that match given trim characters, returns the number of base chars trimmed.
+        // Note: this function only adjusts begin pointer, caller is responsible for adjusting size.
+        template <class Sv_T>
+        static size_t trimFrontImpl(Sv_T& rThis, const Span<const CharT>& trimChars)
+        {
+            if (trimChars.empty())
+                return 0;
+            size_t nTrimCount = 0;
+            auto pRaw = toCharPtr_raw(rThis.m_pFirst);
+            const auto trimCharsRange = makeRange(trimChars.begin(), trimChars.end()); // Needed as trimChars doesn't have cbegin() or cend() which contains() uses (https://stackoverflow.com/questions/62757700/why-does-stdspan-lack-cbegin-and-cend-methods)
+            while (!rThis.empty() && ::DFG_MODULE_NS(alg)::contains(trimCharsRange, *pRaw))
+            {
+                ++pRaw;
+                ++nTrimCount;
+            }
+            rThis.m_pFirst = PtrT(pRaw);
+            return nTrimCount;
         }
 
         PtrT m_pFirst;          // Pointer to first character.
@@ -523,6 +544,24 @@ public:
         return DFG_DETAIL_NS::substr_tailByCount(*this, nTailLength);
     }
 
+    // Removes every base character from front that match given trim characters, returns the number of base chars trimmed.
+    // Implementation note: 
+    //      -Trim chars are taken as raw chars as trim implementation currently works with base characters, not with code points.
+    //      -Using typed view with such implementation would lead to unexpected results, for example in case of UTF8:
+    //          -*this is UTF8 with bytes C3 84 C3 85 (two code points)
+    //          -trim chars is UTF8 view C3 85 (single code point)
+    //          -Trim implementation would result to broken 84 C3 85 as it would remove any of {C3, 85} from front
+    //          -From code point perspective one could expect trimFront with two two-byte codepoints to
+    //           work like e.g. trimFront("ab", "a") by removing first code point.
+    //      -
+    size_t trimFront(const Span<const CharT> svTrimChars)
+    {
+        const auto nTrimCount = this->trimFrontImpl(*this, svTrimChars);
+        DFG_ASSERT_UB(nTrimCount <= this->size());
+        this->m_nSize -= nTrimCount;
+        return nTrimCount;
+    }
+
     const_iterator end() const
     {
         return PtrT(toCharPtr_raw(this->m_pFirst) + this->m_nSize);
@@ -677,6 +716,16 @@ public:
     StringViewSz substr_tailByCount(const size_t nTailLength) const
     {
         return DFG_DETAIL_NS::substr_tailByCount(*this, nTailLength);
+    }
+
+    // For documentation, see corresponding function in StringView.
+    size_t trimFront(const Span<CharT> svTrimChars)
+    {
+        const auto nTrimCount = this->trimFrontImpl(*this, svTrimChars);
+        DFG_ASSERT_UB(nTrimCount <= this->lengthNonCaching());
+        if (isLengthCalculated())
+            this->m_nSize -= nTrimCount;
+        return nTrimCount;
     }
 
     // Returns view as untyped.

dfgTest/dfgTest.cpp
@@ -207,6 +207,7 @@ int main(int argc, char **argv)
     //::testing::GTEST_FLAG(filter) = "dfgStr.StringView_autoConvToUntyped";
     //::testing::GTEST_FLAG(filter) = "dfgStr.StringView_lessThan";
     //::testing::GTEST_FLAG(filter) = "dfgStr.StringViewOrOwner";
+    //::testing::GTEST_FLAG(filter) = "dfgStr.StringView_trimFront";
     //::testing::GTEST_FLAG(filter) = "dfgStr.StringViewSz";
     //::testing::GTEST_FLAG(filter) = "dfgStr.strTo";
     //::testing::GTEST_FLAG(filter) = "dfgStr.strTo_bool";

dfgTest/dfgTestStr.cpp
@@ -1391,6 +1391,39 @@ namespace
 
 }
 
+namespace
+{
+    // Helper for doing some test for every StringView typedef, see existing usages for examples.
+    template <class Handler_T>
+    void forEachStringViewType(Handler_T&& handler)
+    {
+        using namespace DFG_ROOT_NS;
+        handler(StringViewC());
+        handler(StringViewW());
+        handler(StringView16());
+        handler(StringView32());
+        handler(StringViewAscii());
+        handler(StringViewLatin1());
+        handler(StringViewUtf8());
+        handler(StringViewUtf16());
+    }
+
+    // The same as forEachStringViewType but for StringViewSz types.
+    template <class Handler_T>
+    void forEachStringViewSzType(Handler_T&& handler)
+    {
+        using namespace DFG_ROOT_NS;
+        handler(StringViewSzC(""));
+        handler(StringViewSzW(L""));
+        handler(StringViewSz16(u""));
+        handler(StringViewSz32(U""));
+        handler(StringViewSzAscii(SzPtrAscii("")));
+        handler(StringViewSzLatin1(SzPtrLatin1("")));
+        handler(StringViewSzUtf8(SzPtrUtf8("")));
+        handler(StringViewSzUtf16(SzPtrUtf16(u"")));
+    }
+}
+
 TEST(dfgStr, StringView)
 {
     using namespace DFG_ROOT_NS;
@@ -1643,6 +1676,61 @@ TEST(dfgStr, StringView_autoConvToUntyped)
     StringView_autoConvToUntyped_impl<DFG_CLASS_NAME(StringViewW), DFG_CLASS_NAME(StringViewW)>(L"abc");
     StringView_autoConvToUntyped_impl<DFG_CLASS_NAME(StringViewSzW), DFG_CLASS_NAME(StringViewSzW)>(L"abc");
     StringView_autoConvToUntyped_impl<DFG_CLASS_NAME(StringViewSzW), DFG_CLASS_NAME(StringViewW)>(L"abc");
+}
+
+namespace
+{
+    template <class Sv_T, class Char_T>
+    void stringView_trimFrontImpl(const Sv_T trimInput, const Sv_T expected, const Char_T* pszTrimChars)
+    {
+        using namespace DFG_ROOT_NS;
+        Sv_T sv = trimInput;
+        const auto nExpectedTrimCount = sv.size() - expected.size();
+        std::vector<Char_T> trimChars;
+        for (auto p = pszTrimChars; *p != '\0'; ++p)
+            trimChars.push_back(static_cast<Char_T>(*p));
+        const auto nTrimCount = sv.trimFront(trimChars);
+        DFGTEST_EXPECT_LEFT(expected, sv);
+        DFGTEST_EXPECT_LEFT(nExpectedTrimCount, nTrimCount);
+        DFGTEST_EXPECT_LEFT(trimInput.size() - nTrimCount, sv.size());
+    }
+
+} // unnamed namespace
+
+TEST(dfgStr, StringView_trimFront)
+{
+    using namespace DFG_ROOT_NS;
+
+#define DFGTEST_TEMP_DEFINE_FUNC(NAME, INPUT, EXPECTED, TRIMCHARS) \
+    const auto testFunc_##NAME = [](auto sv) \
+    { \
+        using SvT = decltype(sv); \
+        using CharT = typename SvT::CharT; \
+        using SzPtrT = typename SvT::SzPtrT; \
+        const auto trimInput = DFG_STRING_LITERAL_BY_CHARTYPE(CharT, INPUT); \
+        const auto expected = DFG_STRING_LITERAL_BY_CHARTYPE(CharT, EXPECTED); \
+        const auto trimChars = DFG_STRING_LITERAL_BY_CHARTYPE(CharT, TRIMCHARS); \
+        stringView_trimFrontImpl<SvT>(SvT(SzPtrT(trimInput)), SvT(SzPtrT(expected)), trimChars); \
+    }
+
+#define DFGTEST_TEMP_DEFAULT_TRIM_CHARS " \r\n\t"
+    DFGTEST_TEMP_DEFINE_FUNC(empty, "", "", DFGTEST_TEMP_DEFAULT_TRIM_CHARS);
+    DFGTEST_TEMP_DEFINE_FUNC(ws, " \r \n  \t ", "", DFGTEST_TEMP_DEFAULT_TRIM_CHARS);
+    DFGTEST_TEMP_DEFINE_FUNC(abc, " abc ", "abc ", DFGTEST_TEMP_DEFAULT_TRIM_CHARS);
+    DFGTEST_TEMP_DEFINE_FUNC(customTrimChars, "abc ", " ", "bac");
+
+    forEachStringViewType(testFunc_empty);
+    forEachStringViewType(testFunc_ws);
+    forEachStringViewType(testFunc_abc);
+    forEachStringViewType(testFunc_customTrimChars);
+
+    forEachStringViewSzType(testFunc_empty);
+    forEachStringViewSzType(testFunc_ws);
+    forEachStringViewSzType(testFunc_abc);
+    forEachStringViewSzType(testFunc_customTrimChars);
+
+#undef DFGTEST_TEMP_DEFINE_FUNC
+#undef DFGTEST_TEMP_DEFAULT_TRIM_CHARS
 }
 
 namespace

commit 8ca79c60aa150e2a218f4c12e30e92fb3acb5331

[buildfix] ReadOnlySzParam.hpp: inclusion of alg.hpp in f75e6e6d broke building dfgQtTableEditor

dfg/ReadOnlySzParam.hpp
@@ -12,8 +12,8 @@
 #include "str/string.hpp"
 #include "build/languageFeatureInfo.hpp"
 #include <string_view>
-#include "alg.hpp"
 #include "Span.hpp"
+// Note: This is widely used header so includes need care: e.g. including alg.hpp would break compilation of some examples.
 
 DFG_ROOT_NS_BEGIN {
 
@@ -247,8 +247,7 @@ namespace DFG_DETAIL_NS
                 return 0;
             size_t nTrimCount = 0;
             auto pRaw = toCharPtr_raw(rThis.m_pFirst);
-            const auto trimCharsRange = makeRange(trimChars.begin(), trimChars.end()); // Needed as trimChars doesn't have cbegin() or cend() which contains() uses (https://stackoverflow.com/questions/62757700/why-does-stdspan-lack-cbegin-and-cend-methods)
-            while (!rThis.empty() && ::DFG_MODULE_NS(alg)::contains(trimCharsRange, *pRaw))
+            while (!rThis.empty() && std::any_of(trimChars.begin(), trimChars.end(), [&pRaw](const auto c) { return c == *pRaw; }))
             {
                 ++pRaw;
                 ++nTrimCount;
@@ -566,9 +565,8 @@ public:
     // For discussion about behaviour related to base character vs code points, see trimFront()
     size_t trimTail(const Span<const CharT> trimChars)
     {
-        const auto trimCharsRange = makeRange(trimChars.begin(), trimChars.end()); // Needed as trimChars doesn't have cbegin() or cend() which contains() uses (https://stackoverflow.com/questions/62757700/why-does-stdspan-lack-cbegin-and-cend-methods)
         const auto nOrigSize = this->m_nSize;
-        while (!this->empty() && ::DFG_MODULE_NS(alg)::contains(trimCharsRange, *(endRaw() - 1)))
+        while (!this->empty() && std::any_of(trimChars.begin(), trimChars.end(), [this](const auto c) { return c == (*(this->endRaw() - 1)); }))
             --(this->m_nSize);
         return nOrigSize - this->m_nSize;
     }