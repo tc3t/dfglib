tc3t/dfglib

commit c0e1b4b03248b9c12aaf40df212f2d0f3a7a763a

[imp] qt::CsvTableView: Can now copy row number to clipboard from row header context menu

dfg/qt/CsvTableView.cpp
@@ -856,6 +856,7 @@ CsvTableView::CsvTableView(std::shared_ptr<QReadWriteLock> spReadWriteLock, QWid
 
     this->setHorizontalHeader(new TableHeaderView(this));
 
+    this->setVerticalHeader(new TableVerticalHeaderView(this));
     auto pVertHdr = verticalHeader();
     if (pVertHdr)
     {
@@ -6651,6 +6652,59 @@ void TableHeaderView::initStyleOptionForIndex(QStyleOptionHeader* option, const 
     }
 }
 #endif
+
+//////////////////////////////////////////////////////////////////////////
+//
+// class TableVerticalHeaderView
+//
+//////////////////////////////////////////////////////////////////////////
+
+TableVerticalHeaderView::TableVerticalHeaderView(CsvTableView* pParent)
+    : BaseClass(Qt::Vertical, pParent)
+{
+    this->setSectionsClickable(true);
+}
+
+auto TableVerticalHeaderView::tableView() -> CsvTableView*
+{
+    return qobject_cast<CsvTableView*>(parent());
+}
+
+auto TableVerticalHeaderView::tableView() const -> const CsvTableView*
+{
+    return qobject_cast<const CsvTableView*>(parent());
+}
+
+void TableVerticalHeaderView::contextMenuEvent(QContextMenuEvent* pEvent)
+{
+    if (!pEvent)
+        return;
+
+    const auto nViewRow = this->logicalIndexAt(pEvent->pos());
+
+    if (nViewRow < 0)
+        return;
+
+    auto pTableView = tableView();
+    if (!pTableView)
+        return;
+    const auto nRowModel = pTableView->mapRowColToDataModel(nViewRow, 0).first;
+    const auto nRowUser = CsvItemModel::internalRowIndexToVisible(nRowModel);
+
+    QMenu menu;
+    {
+        auto pAct = menu.addAction(tr("Copy row number '%1' to clipboard").arg(nRowUser));
+        if (pAct)
+            connect(pAct, &QAction::triggered, this, [nRowUser]()
+                {
+                    auto pClipboard = QApplication::clipboard();
+                    if (pClipboard)
+                        pClipboard->setText(QString::number(nRowUser));
+                });
+    }
+
+    menu.exec(QCursor::pos());
+}
 
 namespace DFG_DETAIL_NS
 {

dfg/qt/CsvTableView.hpp
@@ -294,7 +294,24 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
 
     public:
         int m_nLatestContextMenuEventColumn_dataModel = -1;
-    };
+    }; // class TableHeaderView
+
+    class TableVerticalHeaderView : public QHeaderView
+    {
+        Q_OBJECT
+    public:
+        using BaseClass = QHeaderView;
+        TableVerticalHeaderView(CsvTableView* pParent);
+
+              CsvTableView* tableView();
+        const CsvTableView* tableView() const;
+
+        // Protected base class overrides -->
+    protected:
+        void contextMenuEvent(QContextMenuEvent* pEvent) override;
+        // <-- Base class overrides
+
+    }; // class TableVerticalHeaderView
 
     class ProgressWidget : public QProgressDialog
     {

commit 817a5275ad324f5fcb6b98beb7a322e06346650c

[fix] qt::CsvTableView: row header wouldn't get highlighted for selected cells.
-Broken in c0e1b4b03248b9c12aaf40df212f2d0f3a7a763a

dfg/qt/CsvTableView.cpp
@@ -6683,6 +6683,7 @@ TableVerticalHeaderView::TableVerticalHeaderView(CsvTableView* pParent)
     : BaseClass(Qt::Vertical, pParent)
 {
     this->setSectionsClickable(true);
+    this->setHighlightSections(true);
 }
 
 auto TableVerticalHeaderView::tableView() -> CsvTableView*