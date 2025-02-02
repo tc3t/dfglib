tc3t/dfglib

commit eb71789083b4f4000165548f07e713c746eff9d9

[imp] qt::TableEditor: added control buttons for cell editor sizing (default and custom)

dfg/qt/TableEditor.cpp
@@ -43,6 +43,7 @@ DFG_BEGIN_INCLUDE_QT_HEADERS
 #include <QSplitter>
 #include <QTimer>
 #include <QVector>
+#include <QInputDialog>
 DFG_END_INCLUDE_QT_HEADERS
 
 #define DFG_TABLEEDITOR_LOG_DEBUG(x)   // Placeholder for logging debug-level message
@@ -149,7 +150,7 @@ namespace
                                               TableEditor,
                                               TableEditorPropertyId_cellEditorHeight,
                                               WindowExtentProperty,
-                                              [] { return WindowExtentProperty(50); },
+                                              [] { return WindowExtentProperty(50); }, // Note: used also as default minimum cell editor size
                                               WindowExtentProperty);
     DFG_QT_DEFINE_OBJECT_PROPERTY("TableEditor_cellEditorFontPointSize",
                                               TableEditor,
@@ -558,7 +559,7 @@ TableEditor::TableEditor()
 
     }
 
-    // Cell Editor and it's container widget
+    // Cell editor, its container and title bar widgets
     {
         m_spCellEditorDockWidget.reset(new QDockWidget(this));
         m_spCellEditorDockWidget->setFeatures(QDockWidget::NoDockWidgetFeatures);
@@ -568,10 +569,84 @@ TableEditor::TableEditor()
         m_spCellEditor->setDisabled(true);
         m_spCellEditor->setFontPointSizeF(getTableEditorProperty<TableEditorPropertyId_cellEditorFontPointSize>(this));
 
+        // Creating custom title bar that has buttons.
+        {
+            auto pTitleBarWidget = new QWidget(this); // Deletion by parenthood
+            auto pTitleBarLayout = new QBoxLayout(QBoxLayout::Direction::LeftToRight, pTitleBarWidget); // Deletion by parenthood
+            pTitleBarLayout->setContentsMargins(0, 0, 0, 0);
+            // Tweaking title bar colour a bit (getting colour of default title bar widget didn't seem to be too easy -> tweaking/hacking the colour manually)
+            {
+                auto titleWidgetPalette = pTitleBarWidget->palette();
+                const auto defaultWindowColour = titleWidgetPalette.color(QPalette::Window);
+                pTitleBarWidget->setStyleSheet(QString("background-color: %1;").arg(defaultWindowColour.darker(110).name()));
+            }
+
+            auto pTextLabel = new QLabel(tr("Cell edit"), this);
+            pTextLabel->setContentsMargins(0, 0, 0, 0);
+
+            // Layout in title bar (horizontal):
+            //    text label, spacer, buttons
+            pTitleBarLayout->addWidget(pTextLabel);
+            pTitleBarLayout->addStretch();
+
+            QPointer<QBoxLayout> spTitleBarLayout = pTitleBarLayout;
+            const auto addButton = [this, &spTitleBarLayout](const QString& sIconPathOrText, const QString sToolTip, auto clickHandler)
+                {
+                    if (!spTitleBarLayout)
+                        return;
+                    auto pButton = new QPushButton(m_spCellEditorDockWidget.get()); // Deletion by parenthood
+                    DFG_QT_VERIFY_CONNECT(connect(pButton, &QPushButton::clicked, m_spCellEditorDockWidget.get(), clickHandler));
+                    if (sIconPathOrText.startsWith(":/"))
+                        pButton->setIcon(QIcon(sIconPathOrText));
+                    else
+                        pButton->setText(sIconPathOrText);
+                    pButton->setContentsMargins(0, 0, 0, 0);
+                    pButton->setToolTip(sToolTip);
+                    pButton->setMaximumWidth(25);
+                    spTitleBarLayout->addWidget(pButton);
+                };
+            // Default size -button
+            addButton(":/resources/defaultSize.png", tr("Set default cell editor height"), [this]()
+                {
+                    DFG_TABLEEDITOR_LOG_DEBUG("CellEditor 'default size'-action triggered");
+                    const auto defaultHeight = getTableEditorProperty<TableEditorPropertyId_cellEditorHeight>(this);
+                    setWidgetMaximumHeight(m_spCellEditorDockWidget.get(), this->height(), defaultHeight);
+                    this->update();
+                });
+            // Resize-button
+            addButton("rz", tr("Set custom cell editor height"), [this]()
+                {
+                    DFG_TABLEEDITOR_LOG_DEBUG("CellEditor 'resize'-action triggered");
+                    if (!m_spCellEditorDockWidget || !m_spTableView)
+                        return;
+                    const auto nCurrentHeight = m_spCellEditorDockWidget->height();
+                    const auto nMinimumSize = getTableEditorProperty<TableEditorPropertyId_cellEditorHeight>(nullptr).toAbsolute(0);
+                    const auto nTableAndEditorHeights = m_spTableView->height() + m_spCellEditorDockWidget->height();
+                    // Note: if want to set height to more than half of tableview+cellEditor, probably need to go through layout.
+                    const auto nMaximumSize = nTableAndEditorHeights / 2;
+                    bool bOk = false;
+                    const auto nNewSize = QInputDialog::getInt(this,
+                        tr("Cell editor height"),
+                        tr("New cell editor height, in range [%1, %2]").arg(nMinimumSize).arg(nMaximumSize),
+                        nCurrentHeight,
+                        nMinimumSize,
+                        nMaximumSize,
+                        1,
+                        &bOk);
+                    if (!bOk)
+                        return;
+                    setWidgetMaximumHeight(m_spCellEditorDockWidget.get(), 0, WindowExtentProperty(nNewSize));
+                    this->update();
+                });
+            pTitleBarWidget->setContentsMargins(0, 0, 0, 0);
+            pTitleBarWidget->setFixedHeight(21);
+            m_spCellEditorDockWidget->setTitleBarWidget(pTitleBarWidget); // Does not take ownership.
+        }
+
         m_spCellEditorDockWidget->setWidget(m_spCellEditor.get());
 
         DFG_QT_VERIFY_CONNECT(connect(m_spCellEditor.get(), &QPlainTextEdit::textChanged, this, &ThisClass::onCellEditorTextChanged));
-    }
+    } // Cell editor widgets
 
     // Status bar
     {

commit c62230e51522afdafc519890fd6b41de3469963c

[fix] qt:TableEditor: Cell editor title text wasn't updating correctly
-Probably broken in eb717890
-Also renamed CellEditor to CellTextEditor

dfg/qt/TableEditor.cpp
@@ -193,14 +193,22 @@ namespace
         return sizes[1];
     }
 
-    static void setCellEditorTitle(QWidget* pWidget, const QString& sAdditionalInfo = QString(), const bool bReadOnly = false)
+    static void setCellEditorTitle(QDockWidget* pWidget, const QString& sAdditionalInfo = QString(), const bool bReadOnly = false)
     {
         if (!pWidget)
             return;
         auto sTitle = pWidget->tr("Cell edit%1").arg((bReadOnly) ? pWidget->tr(" (read-only)") : "");
         if (!sAdditionalInfo.isEmpty())
             sTitle += sAdditionalInfo;
-        pWidget->setWindowTitle(sTitle);
+        auto pTitleBarWidget = qobject_cast<TableEditor::CellEditorWidgetTitleBar*>(pWidget->titleBarWidget());
+        if (pTitleBarWidget)
+        {
+            pTitleBarWidget->setTitleText(sTitle);
+        }
+        else
+        {
+            DFG_ASSERT_CORRECTNESS(false); // Not expected to reach this branch.
+        }
     }
 
 } // unnamed namespace
@@ -475,7 +483,7 @@ DFG_OPAQUE_PTR_DEFINE(TableEditor)
     QObjectStorage<QToolButton> m_spFileInfoButton;
 };
 
-void TableEditor::CellEditor::setFontPointSizeF(const qreal pointSize)
+void TableEditor::CellTextEditor::setFontPointSizeF(const qreal pointSize)
 {
     if (pointSize <= 0)
     {
@@ -487,6 +495,16 @@ void TableEditor::CellEditor::setFontPointSizeF(const qreal pointSize)
     setFont(font);
 }
 
+void DFG_DETAIL_NS::CellEditorWidgetTitleBarImpl::setTitleText(const QString& sText)
+{
+    auto pLabel = findChild<QLabel*>();
+    if (pLabel)
+        pLabel->setText(sText);
+    else
+    {
+        DFG_ASSERT_IMPLEMENTED(false);
+    }
+}
 
 TableEditor::TableEditor()
 {
@@ -565,13 +583,13 @@ TableEditor::TableEditor()
         m_spCellEditorDockWidget->setFeatures(QDockWidget::NoDockWidgetFeatures);
         m_spCellEditorDockWidget->setObjectName("Cell edit");
 
-        m_spCellEditor.reset(new CellEditor(this));
+        m_spCellEditor.reset(new CellTextEditor(this));
         m_spCellEditor->setDisabled(true);
         m_spCellEditor->setFontPointSizeF(getTableEditorProperty<TableEditorPropertyId_cellEditorFontPointSize>(this));
 
         // Creating custom title bar that has buttons.
         {
-            auto pTitleBarWidget = new QWidget(this); // Deletion by parenthood
+            auto pTitleBarWidget = new CellEditorWidgetTitleBar(this); // Deletion by parenthood
             auto pTitleBarLayout = new QBoxLayout(QBoxLayout::Direction::LeftToRight, pTitleBarWidget); // Deletion by parenthood
             pTitleBarLayout->setContentsMargins(0, 0, 0, 0);
             // Tweaking title bar colour a bit (getting colour of default title bar widget didn't seem to be too easy -> tweaking/hacking the colour manually)
@@ -585,9 +603,9 @@ TableEditor::TableEditor()
             pTextLabel->setContentsMargins(0, 0, 0, 0);
 
             // Layout in title bar (horizontal):
-            //    text label, spacer, buttons
+            //    text label, buttons
             pTitleBarLayout->addWidget(pTextLabel);
-            pTitleBarLayout->addStretch();
+            //pTitleBarLayout->addStretch(); // This can be used to add spacer between label and buttons.
 
             QPointer<QBoxLayout> spTitleBarLayout = pTitleBarLayout;
             const auto addButton = [this, &spTitleBarLayout](const QString& sIconPathOrText, const QString sToolTip, auto clickHandler)
@@ -761,7 +779,7 @@ TableEditor::TableEditor()
         spLayout->addWidget(m_spStatusBar.get(), row++, 0);
 
         spLayout->setSpacing(0);
-        auto pEditorWidget = new QWidget(this);
+        auto pEditorWidget = new CellEditorWidget(this);
         delete pEditorWidget->layout();
         pEditorWidget->setLayout(spLayout.release());
         m_spMainSplitter->addWidget(pEditorWidget);

dfg/qt/TableEditor.hpp
@@ -35,6 +35,16 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
     {
         class FindPanelWidget;
         class FilterPanelWidget;
+
+        class CellEditorWidgetTitleBarImpl : public QWidget
+        {
+            Q_OBJECT;
+        public:
+            using BaseClass = QWidget;
+            using BaseClass::BaseClass;
+
+            void setTitleText(const QString& sText);
+        }; // class CellEditorWidgetTitleBar
     }
 
     class TableEditorStatusBar : public QStatusBar
@@ -55,6 +65,7 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
         typedef CsvItemModel ModelClass;
         typedef CsvTableViewSortFilterProxyModel ProxyModelClass;
         typedef CsvTableView ViewClass;
+        using CellEditorWidgetTitleBar = DFG_DETAIL_NS::CellEditorWidgetTitleBarImpl;
         using Index = int; // Should be identical to CsvItemModel::Index
 
         enum ColumnResizeStyle
@@ -63,17 +74,24 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
             ColumnResizeStyle_auto = ColumnResizeStyle_evenlyDistributed    // Lets TableEditor choose style (note: implementation may change).
         };
 
-        class CellEditor : public QPlainTextEdit
+        class CellTextEditor : public QPlainTextEdit
         {
         public:
            typedef QPlainTextEdit BaseClass;
-           CellEditor(QWidget* parent) :
+           CellTextEditor(QWidget* parent) :
                BaseClass(parent)
            {
            }
 
            void setFontPointSizeF(qreal pointSize);
-        };
+        }; // class CellTextEditor
+
+        class CellEditorWidget : public QWidget
+        {
+        public:
+            using BaseClass = QWidget;
+            using BaseClass::BaseClass;
+        }; // class CellEditorWidget
 
         TableEditor();
         ~TableEditor() DFG_OVERRIDE_DESTRUCTOR;
@@ -160,7 +178,7 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
         QObjectStorage<QLineEdit> m_spLineEditSourcePath;
         QObjectStorage<TableEditorStatusBar> m_spStatusBar;
         QObjectStorage<QLabel> m_spSelectionStatusInfo;
-        QObjectStorage<CellEditor> m_spCellEditor;
+        QObjectStorage<CellTextEditor> m_spCellEditor;
         QObjectStorage<QDockWidget> m_spCellEditorDockWidget;
         QObjectStorage<DFG_DETAIL_NS::FindPanelWidget> m_spFindPanel;
         QObjectStorage<DFG_DETAIL_NS::FilterPanelWidget> m_spFilterPanel;