tc3t/dfglib

commit ff128c85f39674971eb1c9c21db20b203e5da671

[mod] cont::TableSz: changed return type of cellCountNonEmpty()

dfg/cont/table.hpp
@@ -644,6 +644,9 @@ DFG_ROOT_NS_BEGIN { DFG_SUB_NS(cont) {
         typedef typename InterfaceTypes_T::StringT StringT;
         using StringViewT = typename InterfaceTypes_T::StringViewT;
 
+        // Type that is guaranteed to be able to hold the number of non-empty cells.
+        using LinearIndexT = IntegerTypeBySizeAndSign<Min(sizeof(size_t), 2 * sizeof(Index_T)), false>::type;
+
         TableSz() : 
             m_emptyString('\0'),
             m_nBlockSize(2048),
@@ -1077,9 +1080,9 @@ DFG_ROOT_NS_BEGIN { DFG_SUB_NS(cont) {
 
         // Returns the number of non-empty cells in the table.
         // Note: if number does not fit to IndexT, return value is unspecified.
-        Index_T cellCountNonEmpty() const
+        LinearIndexT cellCountNonEmpty() const
         {
-            typename std::make_unsigned<IndexT>::type nCount = 0;
+            LinearIndexT nCount = 0;
             forEachFwdColumnIndex([&](Index_T col)
             {
                 forEachFwdRowInColumn(col, [&](const Index_T, const SzPtrR psz)
@@ -1088,7 +1091,7 @@ DFG_ROOT_NS_BEGIN { DFG_SUB_NS(cont) {
                         nCount++;
                 });
             });
-            return saturateCast<IndexT>(nCount);
+            return nCount;
         }
 
         // Returns content storage size in bytes. Note that returned value includes nulls and possibly content from removed cells.

dfgTest/dfgTest.cpp
@@ -154,6 +154,7 @@ int main(int argc, char **argv)
     //::testing::GTEST_FLAG(filter) = "dfgCont.TableCsv_peekCsvFormatFromFile";
     //::testing::GTEST_FLAG(filter) = "dfgCont.TableSz_appendTablesWithMove";
     //::testing::GTEST_FLAG(filter) = "dfgCont.TableSz_insertToFrontPerformance";
+    //::testing::GTEST_FLAG(filter) = "dfgCont.TableSz_cellCountNonEmpty";
     //::testing::GTEST_FLAG(filter) = "dfgCont.TableSz_contentStorageSizeInBytes";
     //::testing::GTEST_FLAG(filter) = "dfgCont.TableSz_addRemoveColumns";
     //::testing::GTEST_FLAG(filter) = "dfgCont.TableSz_insertRowsAt";

dfgTest/dfgTestCont_tables.cpp
@@ -713,6 +713,28 @@ TEST(dfgCont, TableSz_indexHandling)
     }
 }
 
+TEST(dfgCont, TableSz_cellCountNonEmpty)
+{
+    using namespace ::DFG_ROOT_NS;
+    using namespace ::DFG_MODULE_NS(cont);
+
+    {
+        TableSz<char, int8> tablei8;
+        TableSz<char, uint8> tableu8;
+        for (int r = 0; r < 256; ++r)
+        {
+            for (int c = 0; c < 256; ++c)
+            {
+                tablei8.setElement(r, c, "a");
+                tableu8.setElement(r, c, "a");
+            }
+        }
+        // Maximum of index type is not valid, so expecting 127*127 / 255*255 instead of 128*128 / 256*256.
+        DFGTEST_EXPECT_LEFT(127 * 127, tablei8.cellCountNonEmpty());
+        DFGTEST_EXPECT_LEFT(255 * 255, tableu8.cellCountNonEmpty());
+    }
+}
+
 TEST(dfgCont, TableSz_swapCellContent)
 {
     using namespace DFG_MODULE_NS(cont);

commit bdbfed903e119ef8a11f551778681f93fe717dc3

[buildfix] cont::TableSz: fix building on older compilers (broken in ff128c85)

dfg/cont/table.hpp
@@ -645,7 +645,7 @@ DFG_ROOT_NS_BEGIN { DFG_SUB_NS(cont) {
         using StringViewT = typename InterfaceTypes_T::StringViewT;
 
         // Type that is guaranteed to be able to hold the number of non-empty cells.
-        using LinearIndexT = IntegerTypeBySizeAndSign<Min(sizeof(size_t), 2 * sizeof(Index_T)), false>::type;
+        using LinearIndexT = typename IntegerTypeBySizeAndSign<Min(sizeof(size_t), 2 * sizeof(Index_T)), false>::type;
 
         TableSz() : 
             m_emptyString('\0'),