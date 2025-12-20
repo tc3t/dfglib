tc3t/dfglib

commit 536034d1492ef938477b8655ce56df5c0f7ed948

[ref] qt::CsvItemModel: renamed some member functions

dfg/qt/CsvTableView.cpp
@@ -2515,13 +2515,13 @@ bool CsvTableView::saveConfigFileWithOptions()
     int nRow = 0;
     config.forEachKeyValue([&](const StringViewUtf8& svKey, const StringViewUtf8& svValue)
     {
-        configModel.setItem(nRow, 0, svKey);
-        configModel.setItem(nRow, 1, svValue);
+        configModel.setDataNoUndo(nRow, 0, svKey);
+        configModel.setDataNoUndo(nRow, 1, svValue);
         if (bHasExistingConfPath)
         {
             const auto bOldConfigHasKey = existingConfig.contains(svKey);
             const auto existingValue = existingConfig.value(svKey);
-            configModel.setItem(nRow, 2, existingValue);
+            configModel.setDataNoUndo(nRow, 2, existingValue);
             if (bOldConfigHasKey && existingValue != svValue) // If existing value is different from proposed value, highlighting old value in red.
                 configModel.setHighlighter(CsvItemModel::HighlightDefinition(QString("diffHighlighter_%1").arg(nRow), nRow, 2, StringMatchDefinition::makeMatchEverythingMatcher(), QBrush(QColor(255, 0, 0, 64), Qt::SolidPattern)));
         }

dfg/qt/CsvItemModel.cpp
@@ -757,19 +757,19 @@ bool CsvItemModel::saveImpl(Stream_T& strm, const SaveOptions& options)
     return strm.good();
 }
 
-bool CsvItemModel::setItem(const Index nRow, const Index nCol, const StringViewUtf8& sv)
+bool CsvItemModel::setItem_noDataChangedSig(const Index nRow, const Index nCol, const StringViewUtf8& sv)
 {
     const auto bRv = table().addString(sv, nRow, nCol);
     DFG_ASSERT(bRv); // Triggering ASSERT means that string couldn't be added to table.
     return bRv;
 }
 
-bool CsvItemModel::setItem(const Index nRow, const Index nCol, const QString& str)
+bool CsvItemModel::setItem_noDataChangedSig(const Index nRow, const Index nCol, const QString& str)
 {
-    return setItem(nRow, nCol, SzPtrUtf8R(str.toUtf8()));
+    return setItem_noDataChangedSig(nRow, nCol, SzPtrUtf8R(str.toUtf8()));
 }
 
-bool CsvItemModel::clearItem(const Index nRow, const Index nCol)
+bool CsvItemModel::clearItem_noDataChangedSig(const Index nRow, const Index nCol)
 {
     table().clearCell(nRow, nCol);
     return true;
@@ -786,7 +786,7 @@ void CsvItemModel::setRow(const int nRow, QString sLine)
     DFG_MODULE_NS(io)::DelimitedTextReader::readRow<wchar_t>(strm, cSeparator, cEnclosing, L'\n', [&](const size_t nCol, const wchar_t* const p, const size_t nDataLength)
     {
         const auto nSize = (nDataLength > NumericTraits<int>::maxValue) ? NumericTraits<int>::maxValue : static_cast<int>(nDataLength);
-        setItem(nRow, static_cast<int>(nCol), QString::fromWCharArray(p, nSize));
+        setItem_noDataChangedSig(nRow, static_cast<int>(nCol), QString::fromWCharArray(p, nSize));
     });
 
     if (!m_bResetting)
@@ -1756,7 +1756,7 @@ bool CsvItemModel::privSetDataToTable(const int nRow, const int nCol, const Stri
     if (svExisting == sv)
         return false;
 
-    return setItem(nRow, nCol, sv);
+    return setItem_noDataChangedSig(nRow, nCol, sv);
 }
 
 bool CsvItemModel::setDataNoUndo(const Index nRow, const Index nCol, const StringViewUtf8 sv)
@@ -2020,7 +2020,7 @@ void CsvItemModel::setColumnCells(const int nCol, const std::vector<QString>& ve
     const auto nRowCount = Min(getRowCount(), static_cast<int>(vecStrings.size()));
     for (int r = 0; r<nRowCount; ++r)
     {
-        setItem(r, nCol, vecStrings[static_cast<size_t>(r)]);
+        setItem_noDataChangedSig(r, nCol, vecStrings[static_cast<size_t>(r)]);
     }
     if (!m_bResetting)
         Q_EMIT dataChanged(this->index(0, nCol), this->index(getRowCount()-1, nCol));
@@ -2036,9 +2036,9 @@ void CsvItemModel::setColumnCells(const Index nCol, const ColumnContentStorage& 
     {
         // First clearing all cells where there's no item in strings.
         for (; r < item.first; ++r)
-            clearItem(r, nCol);
+            clearItem_noDataChangedSig(r, nCol);
         DFG_ASSERT_CORRECTNESS(r == item.first);
-        setItem(r, nCol, item.second(strings).toStringView());
+        setItem_noDataChangedSig(r, nCol, item.second(strings).toStringView());
         ++r;
     }
     if (!m_bResetting)

dfg/qt/CsvItemModel.hpp
@@ -655,14 +655,6 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
         // as cells of given row @p nRow.
         void setRow(const int nRow, QString sLine);
 
-        // Returns true if item was set, false otherwise.
-        bool setItem(Index nRow, Index nCol, const QString& str);
-        bool setItem(Index nRow, Index nCol, const StringViewUtf8& sv);
-
-        // Clears item at given index.
-        // Note: this is different from setting cell to empty string.
-        bool clearItem(Index nRow, Index nCol);
-
         // Sets data of given index without triggering undo, returns return value of privSetDataToTable()
         // Note: Given model index must be valid; this function does not check it.
         bool setDataNoUndo(const QModelIndex& index, const QString& str);
@@ -816,6 +808,14 @@ DFG_ROOT_NS_BEGIN{ DFG_SUB_NS(qt)
 
         template <class This_T, class Func_T>
         static bool forEachColInfoWhileImpl(This_T& rThis, const LockReleaser& lockReleaser, Func_T&& func);
+
+        // Returns true if item was set, false otherwise.
+        bool setItem_noDataChangedSig(Index nRow, Index nCol, const QString& str);
+        bool setItem_noDataChangedSig(Index nRow, Index nCol, const StringViewUtf8& sv);
+
+        // Clears item at given index.
+        // Note: this is different from setting cell to empty string.
+        bool clearItem_noDataChangedSig(Index nRow, Index nCol);
 
     public:
         QUndoStack* m_pUndoStack;

dfgTestQt/dfgTestCsvItemModel.cpp
@@ -583,7 +583,7 @@ TEST(dfgQt, CsvItemModel_QAbstractItemModelTester)
     model.openFile("testfiles/example4.csv"); // Arbitrary choice of example file
     QAbstractItemModelTester modelTester(&model, QAbstractItemModelTester::FailureReportingMode::Warning);
     model.setSize(model.rowCount() + 1, model.columnCount() + 1);
-    model.setItem(2, 2, "abc");
+    model.setDataNoUndo(2, 2, "abc");
     model.removeColumn(1);
     model.removeRow(1);
     model.transpose();

dfgTestQt/dfgTestCsvTableView.cpp
@@ -424,6 +424,8 @@ TEST(dfgQt, CsvTableView_clear)
     const int nColCount = 4;
     populateModelWithRowColumnIndexStrings(csvModel, nRowCount, nColCount);
 
+    // Signal count tests check that get expect number of dataChanged-signals, for example if clearing
+    // all cells in a column, expecting one signal covering whole column instead of signal for every cell.
     size_t nSignalCount = 0;
     DFG_QT_VERIFY_CONNECT(QObject::connect(&csvModel, &CsvItemModel::dataChanged, [&]() { ++nSignalCount; }));
 
@@ -2035,12 +2037,12 @@ TEST(dfgQt, CsvTableView_trimCells)
     ::DFG_MODULE_NS(qt)::CsvTableWidget view;
     auto& rModel = view.getCsvModel();
     view.resizeTableNoUi(3, 2);
-    rModel.setItem(0, 0, " a  ");
-    rModel.setItem(0, 1, "   b");
-    rModel.setItem(1, 0, "c \t c");
-    rModel.setItem(1, 1, "d   ");
-    rModel.setItem(2, 0, "\te");
-    rModel.setItem(2, 1, " \t f\t\t     \t");
+    rModel.setDataNoUndo(0, 0, " a  ");
+    rModel.setDataNoUndo(0, 1, "   b");
+    rModel.setDataNoUndo(1, 0, "c \t c");
+    rModel.setDataNoUndo(1, 1, "d   ");
+    rModel.setDataNoUndo(2, 0, "\te");
+    rModel.setDataNoUndo(2, 1, " \t f\t\t     \t");
     DFGTEST_EXPECT_LEFT(3, view.getCsvModel().getRowCount());
     DFGTEST_EXPECT_LEFT(2, view.getCsvModel().getColumnCount());
 

commit 2f4d05fdf148e2158c4e3654d0dfc15a85ad9582

[fix] qt::CsvTableView: saveConfigFileWithOptions() didn't populate "Existing value"-column
-Broken in 536034d1

dfg/qt/CsvTableView.cpp
@@ -2506,11 +2506,6 @@ bool CsvTableView::saveConfigFileWithOptions()
     configModel.insertColumns(0, (bHasExistingConfPath) ? 3 : 2);
     configModel.setColumnName(0, tr("Key"));
     configModel.setColumnName(1, tr("Value"));
-    if (bHasExistingConfPath)
-    {
-        configModel.setColumnName(2, tr("Existing value"));
-        configModel.setColumnProperty(2, CsvItemModelColumnProperty::readOnly, true);
-    }
 
     int nRow = 0;
     config.forEachKeyValue([&](const StringViewUtf8& svKey, const StringViewUtf8& svValue)
@@ -2527,6 +2522,12 @@ bool CsvTableView::saveConfigFileWithOptions()
         }
         ++nRow;
     });
+
+    if (bHasExistingConfPath)
+    {
+        configModel.setColumnName(2, tr("Existing value"));
+        configModel.setColumnProperty(2, CsvItemModelColumnProperty::readOnly, true);
+    }
 
     viewDlg.setModel(&configModel);
 